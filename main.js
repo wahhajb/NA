6432a72ac7e218c111f5ff1fb1ca552110928cb5
Mahdikurosaki <154265304+Mahdikurosaki@users.noreply.github.com>
2024-02-26 00:02:16 +0000

    Update main.js

--- main.js
+++ main.js
@@ -543,7 +543,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime}`;
+  const bio = `[ 🍊 ] وقت تشغيل:: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
