3bc231536e37632e1c569c50029f3e42ea1c049b
Mahdikurosaki <154265304+Mahdikurosaki@users.noreply.github.com>
2024-02-27 10:52:55 +0000

    Update convertidor-toanime.js

--- plugins/convertidor-toanime.js
+++ plugins/convertidor-toanime.js
@@ -2,7 +2,7 @@ import uploadImage from '../lib/uploadImage.js';
 const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || q.mediaType || '';
-  if (!/image/g.test(mime)) throw '*[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*';
+  if (!/image/g.test(mime)) throw '*[❗] اعمل ريبلاي على صورة *';
   m.reply('*[❗] تحويل الصورة إلى تصميم أنيمي، كن صبورًا بينما أرسل النتيجة*');
   const data = await q.download?.();
   const image = await uploadImage(data);
