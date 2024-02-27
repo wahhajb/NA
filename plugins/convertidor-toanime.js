3febc1ea31ffef3dbcb71e1bf98ab13db0c2ac3f
Mahdikurosaki <154265304+Mahdikurosaki@users.noreply.github.com>
2024-02-27 10:48:53 +0000

    Update convertidor-toanime.js

--- plugins/convertidor-toanime.js
+++ plugins/convertidor-toanime.js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   const q = m.quoted ? m.quoted : m;
   const mime = (q.msg || q).mimetype || q.mediaType || '';
   if (!/image/g.test(mime)) throw '*[❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙰 𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴 𝙰 𝚄𝙽𝙰 𝙸𝙼𝙰𝙶𝙴𝙽*';
-  m.reply('*[❗] 𝙲𝙾𝙽𝚅𝙸𝙴𝚁𝚃𝙸𝙴𝙽𝙳𝙾 𝙸𝙼𝙰𝙶𝙴𝙽 𝙰 𝙳𝙸𝚂𝙴𝙽̃𝙾 𝙰𝙽𝙸𝙼𝙴, 𝚂𝙴𝙰 𝙿𝙰𝙲𝙸𝙴𝙽𝚃𝙴 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝙴𝙻 𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾*');
+  m.reply('*[❗] تحويل الصورة إلى تصميم أنيمي، كن صبورًا بينما أرسل النتيجة*');
   const data = await q.download?.();
   const image = await uploadImage(data);
   try {
@@ -18,12 +18,12 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
         const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
         await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
       } catch (e) {
-        throw '*[❗] 𝙴𝚁𝚁𝙾𝚁, 𝚅𝙴𝚁𝙸𝙵𝙸𝚀𝚄𝙴 𝚀𝚄𝙴 𝙴𝙽 𝙻𝙰 𝙸𝙼𝙰𝙶𝙴𝙽 𝚂𝙴𝙰 𝚅𝙸𝚂𝙸𝙱𝙻𝙴 𝙴𝙻 𝚁𝙾𝚂𝚃𝚁𝙾 𝙳𝙴 𝚄𝙽𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰*';
+        throw '*[❗] خطأ، تحقق من أن الوجه شخص مرئي في الصورة*';
       }
     }
   }
 };
 handler.help = ['toanime'];
 handler.tags = ['tools'];
-handler.command = /^(jadianime|toanime)$/i;
+handler.command = /^(jadianime|الى_انمي)$/i;
 export default handler;
