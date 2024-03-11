2706c3543d68dd3051844683e96d3371ff087465
Mahdikurosaki <154265304+Mahdikurosaki@users.noreply.github.com>
2024-03-11 06:09:35 -0400

    Update audio3-me.js

--- plugins/audio3-me.js
+++ plugins/audio3-me.js
@@ -3,9 +3,9 @@ const handler = (m) => m;
 handler.all = async function(m) {
   const vn = './media/me.mp3';
   const chat = global.db.data.chats[m.chat];
-  if (/^لوفي$/i.test(m.text) && !chat.isBanned) {
+  if (/^يوهان$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*اهلا, ¿انا لوفي?*`);
+    await m.reply(`*رمضان ,كريم*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
   return !0;
