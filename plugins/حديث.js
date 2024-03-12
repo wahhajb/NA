b5c1bfc1ab8ef4683578a1d29ff2d4241154437a
Mahdikurosaki <154265304+Mahdikurosaki@users.noreply.github.com>
2024-03-12 14:09:44 +0000

    Update ⁨حديث.js⁩

---
+++ plugins/حديث.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+async function handler(m, { conn, text }) {
+  try {
+    const response = await axios.get('https://api3.islamhouse.com/v3/paV29H2gm56kvLPy/main/sitecontent/ar/ar/json');
+    const data = response.data;
+    console.log(data);
+    conn.sendMessage(m.chat, JSON.stringify(data), { quoted: m });
+  } catch (error) {
+    console.error('حدث خطأ:', error);
+    conn.sendMessage(m.chat, 'حدث خطأ أثناء جلب البيانات', { quoted: m });
+  }
+}
+
+handler.command = ['احاديث', 'hadiths'];
+handler.tags = ['islam'];
+
+module.exports = handler;
