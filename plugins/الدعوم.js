2ca7024f6fccf75fcf9a57e4ff827eaaeaa640ce
gigigiuhi <154265304+Mahdikurosaki@users.noreply.github.com>
2024-03-19 17:02:33 +0000

    Update الدعوم.js

--- plugins/الدعوم.js
+++ plugins/الدعوم.js
@@ -5,14 +5,14 @@ let img = await(await fetch('https://chat.whatsapp.com/BxBsz6WSLiIK13w2NtWKJm'))
 let _uptime = process.uptime() * 1000
 let uptime = clockString(_uptime)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let str = `╭────『👑┇HANRY┇🤖┇BOT┇👑』
+let str = `╭────『👑┇JOHAN┇🤖┇BOT┇👑』
 │
 │ *➤ مرحبا ${taguser}*
 │
 │ *=> 🤖 وقت عمل البوت:* ${uptime}
 │ *=> ✅ البوت عام للجميع*
-│ *=> 👑 تم تطويري بواسطة سينتشي*
-│ *=> 🔗 رقم المطور:* *https://wa.me/+905360504045*
+
+│ *=> 🔗 رقم المطور:* *https://wa.me/+22247072475*
 ╰────────────────`.trim()
 let buttons = [{ buttonId: '#menu', buttonText: { displayText: 'قائمة الاوامر 📜' }, type: 1 }]
 let buttonMessage = { image: pp, caption: str.trim(), mentions: [m.sender], footer: global.wm, buttons: buttons, headerType: 4, contextInfo: { mentionedJid: [m.sender], externalAdReply: { showAdAttribution: true, mediaType: 'VIDEO', mediaUrl: null, title: 'اضغط هنا لدخول قروب الدعم', body: 'بواسطة صاصا', thumbnail: img, sourceUrl: `https://chat.whatsapp.com/BxBsz6WSLiIK13w2NtWKJm`}}}
@@ -20,7 +20,7 @@ conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 } catch {
 let _uptime = process.uptime() * 1000
 let uptime = clockString(_uptime)    
-throw `*🤖 وقت العمل: ${uptime} ┃ 👑 بواسطة سينتشي ┃ 🔗 قروب الدعم:*https://chat.whatsapp.com/IqinkrUsBHzCWX8DdvUHLv*`}}
+throw `*🤖 وقت العمل: ${uptime} ┃ LOOK LIKE YOU*`}}
 handler.help = ['estado']
 handler.tags = ['main']
 handler.command = /^(الدعوم|ظعم)
