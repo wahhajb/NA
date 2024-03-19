36ed6d297ebb4369b4dad1c41b6f407c5db578c0
gigigiuhi <154265304+Mahdikurosaki@users.noreply.github.com>
2024-03-19 16:40:42 +0000

    Delete plugins directory

--- plugins/menus-menu.js
+++
@@ -1,100 +0,0 @@
-import { createHash } from 'crypto'
-import { canLevelUp, xpRange } from '../lib/levelling.js'
-import fetch from 'node-fetch'
-import fs from 'fs'
-const { levelling } = '../lib/levelling.js'
-import moment from 'moment-timezone'
-import { promises } from 'fs'
-import { join } from 'path'
-const time = moment.tz('Egypt').format('HH')
-let wib = moment.tz('Egypt').format('HH:mm:ss')
-//import db from '../lib/database.js'
-
-let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
-    let d = new Date(new Date + 3600000)
-    let locale = 'ar'
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
-    let _uptime = process.uptime() * 1000
-    let uptime = clockString(_uptime)
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-    let videoUrl1 = 'https://telegra.ph/file/752713f57db2283356797.mp4';
-    let videoUrl2 = 'https://telegra.ph/file/7b74465ddc6bfa00a84ff.mp4';
-    let vn = './media/menu.mp3';
-    const user = global.db.data.users[m.sender];
-    const {money, joincount} = global.db.data.users[m.sender];
-    const {exp, limit, level, role} = 
-        global.db.data.users[m.sender];
-    let { min, xp, max } = xpRange(user.level, global.multiplier)
-    let username = conn.getName(who)
-    let math = max - xp
-    let sn = createHash('md5').update(who).digest('hex')
-    let totalreg = Object.keys(global.db.data.users).length;
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
-    let more = String.fromCharCode(8206)
-    let readMore = more.repeat(900) 
-    const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
-    let str = ` 
-  ▢ *مرحبا يا,* ${taguser}
-  
- 🎯* احصائياتك *
- → *مستوى:* ${level}
- → *نقاطك:* ${exp}
- → *رتبة:* ${role}
- → *ألماس:* ${limit}
- → *عملك:* ${money}
- → *توكن:* ${joincount}
- → *مميز:* ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌') || ''}
- ${readMore}
- *مرحبا بك في قائمة توضيحية للأوامر في بوت سانكارا*
- 
-».🎗️*تحويلات*
-».🎡*tagall*
-».🎐*hidetag*
-».🎶*play*
-».🎞️play2
-».👓*المطور*
-»...*coming soon*
-    `.trim();
-
-    // اختيار عشوائي بين الرابطين
-    const randomVideoUrl = Math.random() < 0.5 ? videoUrl1 : videoUrl2;
-
-    conn.sendMessage(m.chat, {
-        video: { url: randomVideoUrl }, caption: str,
-        mentions: [m.sender,global.conn.user.jid],
-        gifPlayback: true,gifAttribution: 0
-    }, { quoted: m });
-}; 
-
-handler.help = ['main']
-handler.tags = ['group']
-handler.command = ['اوامر'] 
-
-export default handler
-
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
-
-function ucapan() {
-    const time = moment.tz('Egypt').format('HH')
-    let res = "بداية يوم سعيده ☀️"
-    if (time >= 4) {
-        res = "صباح الخير 🌄"
-    }
-    if (time >= 10) {
-        res = "مساء الخير ☀️"
-    }
-    if (time >= 15) {
-        res = "مساء الخير 🌇"
-    }
-    if (time >= 18) {
-        res = "مساء الخير 🌙"
-    }
-    return res
-}
