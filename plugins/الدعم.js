9dd5f1002ef8424d4cf42da1d70e5ff0f3d7ce4f
gigigiuhi <154265304+Mahdikurosaki@users.noreply.github.com>
2024-03-19 17:01:09 +0000

    Update الدعم.js

--- plugins/الدعم.js
+++ plugins/الدعم.js
@@ -3,33 +3,12 @@ let handler = async (m, {command, conn, usedPrefix}) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/socona12/TheMystic-Bot-MD/master/src/JSON/anime-Venom.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]    
 conn.sendFile(m.chat, haha, 'error.jpg', `
-.⬪  ࣪    ࣭     ࣪     ࣭   𝅦𝅦   ࣭     ࣪     ࣭    ࣪  ⬫
-   ⬪࣪ꥈ𑁍⃪࣭۪ٜ݊݊݊݊݊໑ٜ࣪▭‌໋‌  ۫ ⬧𝑾𝑬𝑳𝑪𝑶𝑴𝑬⬧  ۫  ▭‌໋‌۪۬໑⃪࣭۪ٜ݊݊݊݊𑁍ꥈ࣪⬪ll
-
-*◉═══ • ❁ 👑 ❁ • ═══◉*
-
-w乇ﾚcoﾶ乇 ｲo 乃oｲ ん乇刀尺ﾘ
-
-乃ﾘ ﾶo丂ｲﾑｷﾑ ﾶoんﾑﾶ乇d
-
-*◉═══ • ❁ 👑 ❁ • ═══◉*
-
-*『 ️مرحبا بك في بوت هنري 』*
 
 *『 ️اليك قائمه بسورس البوت البوت 』*
 
-*◉═══ • ❁ 👑 ❁ • ═══◉*
-
-*⊱≼ تم تطويري وبرمجتي ≽⊰⊹*
-*⊱≼ بواسطه احمد سينتشي ≽⊰⊹*
-*⊱≼ اذا كنت تريد صناعه بوت ≽⊰⊹*
-*⊱≼ فعليك الانضمام الي قناتنا ≽⊰⊹*
-
-*◉═══ • ❁ 👑 ❁ • ═══◉*
-
 *『 ️واتساب 』*
 
-*⊱≼ https://wa.me/+905360504045 ≽⊰⊹*
+*⊱≼ https://wa.me/+22247072475 ≽⊰⊹*
 
 *『 ️منصاتي 』*
 
@@ -37,7 +16,7 @@ w乇ﾚcoﾶ乇 ｲo 乃oｲ ん乇刀尺ﾘ
 
 *『 ️جروب واتساب 』*
 
-*⊱≼ https://chat.whatsapp.com/IqinkrUsBHzCWX8DdvUHLv ≽⊰⊹*
+*⊱≼ SOON ≽⊰⊹*
 
 *◉═══ • ❁ 👑 ❁ • ═══◉* `, m)
 }
