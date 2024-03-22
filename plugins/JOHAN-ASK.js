27084c6fb963aad2f3fa1881d04c29a5ded9ae76
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-03-22 13:44:09 +0000

    Update JOHAN-ASK.js

--- plugins/JOHAN-ASK.js
+++ plugins/JOHAN-ASK.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tekateki[id][0])
         throw false
     }
-    let tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`))
+    let tekateki = JSON.parse(fs.readFileSync(`./src/game/dean.json`))
     let json = tekateki[Math.floor(Math.random() * tekateki.length)]
     let _clue = json.response
     let clue = _clue.replace(/[A-Za-z]/g, '_')
