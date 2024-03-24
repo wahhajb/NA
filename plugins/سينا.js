303238d4a739ad9dc25f4937a1c5e65394112169
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-03-24 12:55:59 +0000

    Update ⁨سينا.js⁩

--- plugins/سينا.js
+++ plugins/سينا.js
@@ -12,7 +12,7 @@ let text
         text = args.slice(0).join(" ")
     } else if (m.quoted && m.quoted.text) {
         text = m.quoted.text
-    } else throw "مثال \n .هنري بوت صاصا"
+    } else throw "مثال \n .يوهان بوت "
    if (!text) return m.reply('𝙏𝙀𝙓𝙏E?')
    if (text.length > 30) return m.reply('𝙈𝘼𝙓𝙄𝙈UN 30 𝙋!')
     let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/3387ec330bbd16d82195e.jpg')
