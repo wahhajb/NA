2f5f219b8fdb1e1a2d5e5e8673f9f179a84e3754
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-03-24 12:53:57 +0000

    Update ⁨تقرير.js⁩

--- plugins/تقرير.js
+++ plugins/تقرير.js
@@ -3,7 +3,7 @@ if (!text) throw `*[❗] نسيت التقرير - البلاغ*\n\n*مثال:*\
 if (text.length < 10) throw `*[❗] يجب أن يتكون التقرير من 10 أحرف على الأقل!*`
 if (text.length > 1000) throw `*[❗] التقرير يجب الا يزيد عن 1000حرف!*`
 let teks = `*❒═════[ تحذير ]═════❒*\n*┬*\n*├❧ الرقم:* wa.me/+22247072475{m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ رسالة:* ${text}\n*┴*`
-conn.reply('wa.me/201553631779', m.quoted ? teks + m.quoted.text : teks, null, {
+conn.reply('wa.me/22247072475', m.quoted ? teks + m.quoted.text : teks, null, {
 contextInfo: {
 mentionedJid: [m.sender]
 }})
