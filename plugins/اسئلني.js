74d3ba0daa2dd272b8329271444fce053db628b6
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-03-24 12:57:30 +0000

    Update ⁨اسئلني.js⁩

--- plugins/اسئلني.js
+++ plugins/اسئلني.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-conn.reply(m.chat,`*┌────「 سؤال 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 هـنـري 」─*`, m)
+conn.reply(m.chat,`*┌────「 سؤال 」─*\n*“${pickRandom(global.bucin)}”*\n*└────「 يوهان 」─*`, m)
 }
 handler.help = ['reto']
 handler.tags = ['fun']
