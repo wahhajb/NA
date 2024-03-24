6bbdfe9785ce021db971e4138aa08ffa13b5a34d
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-03-24 12:53:38 +0000

    Update ⁨تقرير.js⁩

--- plugins/تقرير.js
+++ plugins/تقرير.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `*[❗] نسيت التقرير - البلاغ*\n\n*مثال:*\n*${usedPrefix + command} الامر ${usedPrefix}كيريتو عمكم*`
 if (text.length < 10) throw `*[❗] يجب أن يتكون التقرير من 10 أحرف على الأقل!*`
 if (text.length > 1000) throw `*[❗] التقرير يجب الا يزيد عن 1000حرف!*`
-let teks = `*❒═════[ تحذير ]═════❒*\n*┬*\n*├❧ الرقم:* wa.me/+201553631779${m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ رسالة:* ${text}\n*┴*`
+let teks = `*❒═════[ تحذير ]═════❒*\n*┬*\n*├❧ الرقم:* wa.me/+22247072475{m.sender.split`@`[0]}\n*┴*\n*┬*\n*├❧ رسالة:* ${text}\n*┴*`
 conn.reply('wa.me/201553631779', m.quoted ? teks + m.quoted.text : teks, null, {
 contextInfo: {
 mentionedJid: [m.sender]
