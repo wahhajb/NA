d318e7dadc956b7c45db5227ca8e37da70baad6d
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-03-27 11:54:35 +0000

    Update ⁨حديث.js⁩

--- plugins/حديث.js
+++ plugins/حديث.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text}) => {
-m.reply(`*❆❯━━━❲𝗦𝗔𝗦𝗔️❳━━━━━❮❆*\n➢ *"${pickRandom(global.piropo)}"*\n*❆❯━━━❲𝗦𝗔𝗦𝗔️❳━━━━━❮❆*`)
+m.reply(`*❆❯━━━❲hanry❳━━━━━❮❆*\n➢ *"${pickRandom(global.piropo)}"*\n*❆❯━━━❲hanry️❳━━━━━❮❆*`)
 }
 handler.tags = ['احاديث']
 handler.command = ['حديث','احاديث']
