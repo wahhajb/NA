9729141081c78b5e43fab67538b535022986bc6f
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-03-27 17:47:13 +0000

    Update JOHAN-bot2.js

---
+++ plugins/JOHAN-bot2.js
@@ -0,0 +1,15 @@
+let handler = async (m, { conn }) => {
+let done = '🩶'; 
+       m.react(done);
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `تقصد نفسك`;
+
+    conn.sendFile(m.chat, 'تحط الصوره هنا تليجرام', 'image.jpg', message, m);
+};
+
+handler.customPrefix = /^(bot|بوت)$/i;
+handler.command = new RegExp;
+
+export default handler;
