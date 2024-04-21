5c65387c58a54b09b18dc676aa35358c016ec0af
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-04-21 10:45:30 +0000

    Update JOHAN-ggg.js

--- plugins/JOHAN-ggg.js
+++ plugins/JOHAN-ggg.js
@@ -5,13 +5,13 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
 
-  const tradutor = _translate.plugins.random_waifu
+  
 
   const res = await fetch('https://api.waifu.pics/sfw/waifu');
   if (!res.ok) throw await res.text();
   const json = await res.json();
   if (!json.url) throw 'Error!';
-  conn.sendFile(m.chat, json, 'error.jpg', tradutor.texto1, m);
+  conn.sendFile(m.chat, json, 'error.jpg', , m);
 // conn.sendButton(m.chat, `𝙰-𝙰𝚁𝙰 𝙰𝚁𝙰 𝚂𝙴𝙼𝙿𝙰𝙸~~`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 };
 handler.help = ['waifu'];
