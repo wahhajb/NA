f742d48cc161c1937ea7af24abf1ddab972785ae
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-04-22 18:44:29 +0000

    Update JOHAN-ggg.js

--- plugins/JOHAN-ggg.js
+++ plugins/JOHAN-ggg.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   let johan = (هلا انا يوهان)
   let Images = (
   conn.sendFile(m.chat,johan , '', , m);
-// conn.sendButton(m.chat, `𝙰-𝙰𝚁𝙰 𝙰𝚁𝙰 𝚂𝙴𝙼𝙿𝙰𝙸~~`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+  
 };
 handler.help = ['waifu'];
 handler.tags = ['anime'];
