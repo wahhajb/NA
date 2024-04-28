537c4c8f7866a03f8ae5298d708c314b24ab2033
gigigiuhi <154265304+yayuii@users.noreply.github.com>
2024-04-28 21:36:00 +0000

    Update JOHAN-bot2.js

--- plugins/JOHAN-bot2.js
+++ plugins/JOHAN-bot2.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn }) => {
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
     let message = `تقصد نفسك`;
-    
+    let x = { react: { text: '💀', key: m.key };
 
-    conn.sendFile(m.chat, 'https://telegra.ph/file/caafdb2292db8180b99fe.mp4', 'video.mp4',{ react: { text: '💀', key: m.key } , message, m);
+    conn.sendFile(m.chat, 'https://telegra.ph/file/caafdb2292db8180b99fe.mp4', 'video.mp4',x, , message, m);
 };
 
 handler.customPrefix = /^(bot|بوت)$/i;
