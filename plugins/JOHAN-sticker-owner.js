5efa593a47bb15afb53a831c2a2fdb10f70e109e
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 18:58:34 +0300

    Update JOHAN-sticker-owner.js

--- plugins/JOHAN-sticker-owner.js
+++ plugins/JOHAN-sticker-owner.js
@@ -1,5 +1,5 @@
 const handler = async (m, {conn}) => {
-  conn.sendFile(m.chat, `https://telegra.ph/file/ea03bbc1d2dbd77af8a57.jpg`, 'error.webp', '', m, {asSticker: true});
+  conn.sendFile(m.chat,'error.webp', '', m, {asSticker: true});
 };
-handler.command = ['dado', 'dados', 'dadu'];
+handler.command = ['نرد', 'dados', 'dadu'];
 export default handler;
