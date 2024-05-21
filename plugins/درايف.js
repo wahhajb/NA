f13ba88a365ef2a7bbe27edc5e5366d17390a5ad
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 18:19:27 +0300

    Create درايف.js

---
+++ plugins/درايف.js
@@ -0,0 +1,23 @@
+
+import fg from 'api-dylux' 
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+
+	if (!args[0]) throw `✳️ حط رابط من جوجل درايف`
+	m.react(rwait) 
+	try {
+	let res = await fg.GDriveDl(args[0])
+	 await m.reply(`*جاري التنزيل*`)
+		
+	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
+	m.react(done)
+   } catch {
+	m.reply('❌ حصل خطاء يرجى المحاوله مع رابط اخر') 
+  }
+}
+handler.help = ['gdrive']
+handler.tags = ['downloader']
+handler.command = ['درايف']
+handler.credit = false
+handler.premium = false
+
+export default handler
