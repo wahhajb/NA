6800861c209c870a4502f75a743dbf05f2ce8462
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 17:11:13 +0300

    Update JOHAN-bot.js

--- plugins/JOHAN-bot.js
+++ plugins/JOHAN-bot.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/e15e2a8a4e834e7e144db.mp4';
+let videoUrl = 'https://telegra.ph/file/35eabb610d9995bc934b8.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
