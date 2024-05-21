00925956aca8678ba6f6cf0b06e4e2fccc0ec171
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 18:53:56 +0300

    Update JOHAN-quran.js

--- plugins/JOHAN-quran.js
+++ plugins/JOHAN-quran.js
@@ -1,17 +1,140 @@
-import axios from 'axios';
+let handler = async (m, { conn, usedPrefix, command}) => {
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+let pp = './Assets/quran.jpg'
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850) 
+let lkr = `
+      ❀° ┄──•••───╮
+         ♥️ *القرآن الكريم* ♥
+      ╰───•••──┄ °❀
+      
+    *💫 يستخدم الامر لإرسال السور من القران الگريم💫*
+مثال: .سوره 44
 
- async function handler(m, { conn, text }) {
-   if (!text) throw ' يُرجى إدخال رقمين مفصولين بعلامة (+)\n مثال: 1+1'
-   if (!text.includes('+')) throw 'مثال:\n.قران 1+2\n\n𝙱𝚢 : hanry - 𝚋𝚘𝚝'  
-   let [a, b] = text.split('+')   
-   let lr = (`https://api.lolhuman.xyz/api/quran/audio/${a}/${b}?apikey=GataDios`)
-   let res = await axios.get(lr, { responseType: 'arraybuffer' })
-   if (res.status !== 200) throw `حدث خطأ أثناء جلب الرسالة الصوتية.`
-   conn.sendMessage(m.chat, { audio: Buffer.from(res.data, 'binary'), mimetype: 'audio/mpeg' }, { quoted: m });
- }
+1. Al-Fatiha  (The Opening)
+2. Al-Baqarah  (The Cow)
+3. Aali Imran  (The Family of Imran)
+4. An-Nisa'  (The Women)
+5. Al-Ma'idah  (The Table Spread)
+6. Al-An'am  (The Cattle)
+7. Al-A'raf  (The Heights)
+8. Al-Anfal  (The Spoils of War)
+9. At-Tawbah  (The Repentance)
+10. Yunus  (Jonah)
+11. Hud  (Hud)
+12. Yusuf  (Joseph)
+13. Ar-Rad  (The Thunder)
+14. Ibrahim  (Abraham)
+15. Al-Hijr  (The Rocky Tract)
+16. An-Nahl  (The Bee)
+17. Al-Isra'  (The Night Journey)
+18. Al-Kahf  (The Cave)
+19. Maryam  (Mary)
+20. Ta-Ha  (Ta-Ha)
+21. Al-Anbiya'  (The Prophets)
+22. Al-Hajj  (The Pilgrimage)
+23. Al-Mu'minun  (The Believers)
+24. An-Nur  (The Light)
+25. Al-Furqan  (The Criterion)
+26. Ash-Shu'ara'  (The Poets)
+27. An-Naml  (The Ant)
+28. Al-Qasas  (The Stories)
+29. Al-Ankabut  (The Spider)
+30. Ar-Rum  (The Romans)
+31. Luqman  (Luqman)
+32. As-Sajda  (The Prostration)
+33. Al-Ahzab  (The Combined Forces)
+34. Saba'  (Sheba)
+35. Fatir  (The Originator)
+36. Ya-Sin  (Ya-Sin)
+37. As-Saffat  (Those who set the Ranks)
+38. Sad  (The Letter Sad)
+39. Az-Zumar  (The Troops)
+40. Ghafir  (The Forgiver)
+41. Fussilat  (Explained in Detail)
+42. Ash-Shura  (Consultation)
+43. Az-Zukhruf  (The Gold Adornments)
+44. Ad-Dukhan  (The Smoke)
+45. Al-Jathiyah  (The Crouching)
+46. Al-Ahqaf  (The Wind-Curved Sandhills)
+47. Muhammad  (Muhammad)
+48. Al-Fath  (The Victory)
+49. Al-Hujurat  (The Rooms)
+50. Qaf  (The Letter Qaf)
+51. Adh-Dhariyat  (The Winnowing Winds)
+52. At-Tur  (The Mount)
+53. An-Najm  (The Star)
+54. Al-Qamar  (The Moon)
+55. Ar-Rahman  (The Beneficent)
+56. Al-Waqi'a  (The Inevitable)
+57. Al-Hadid  (The Iron)
+58. Al-Mujadila  (The Pleading Woman)
+59. Al-Hashr  (The Exile)
+60. Al-Mumtahanah  (She that is to be examined)
+61. As-Saff  (The Ranks)
+62. Al-Jumu'ah  (Friday)
+63. Al-Munafiqun  (The Hypocrites)
+64. At-Taghabun  (Mutual Disillusion)
+65. At-Talaq  (The Divorce)
+66. At-Tahrim  (The Prohibition)
+67. Al-Mulk  (The Sovereignty)
+68. Al-Qalam  (The Pen)
+69. Al-Haqqah  (The Reality)
+70. Al-Ma'arij  (The Ascending Stairways)
+71. Nuh  (Noah)
+72. Al-Jinn  (The Jinn)
+73. Al-Muzzammil  (The Enshrouded One)
+74. Al-Muddathir  (The Cloaked One)
+75. Al-Qiyamah  (The Resurrection)
+76. Al-Insan  (Man)
+77. Al-Mursalat  (The Emissaries)
+78. An-Naba'  (The Tidings)
+79. An-Nazi'at  (Those who drag forth)
+80. Abasa  (He frowned)
+81. At-Takwir  (The Overthrowing)
+82. Al-Infitar  (The Cleaving)
+83. Al-Mutaffifin  (Defrauding)
+84. Al-Inshiqaq  (The Splitting Open)
+85. Al-Buruj  (The Mansions of the Stars)
+86. At-Tariq  (The Morning Star)
+87. Al-Ala  (The Most High)
+88. Al-Ghashiyah  (The Overwhelming)
+89. Al-Fajr  (The Dawn)
+90. Al-Balad  (The City)
+91. Ash-Shams  (The Sun)
+92. Al-Lail  (The Night)
+93. Ad-Duha  (The Morning Hours)
+94. As-Sharh  (The Relief)
+95. At-Tin  (The Fig)
+96. Al-Alaq  (The Clot)
+97. Al-Qadr  (The Power)
+98. Al-Bayyinah  (The Clear Proof)
+99. Az-Zalzalah  (The Earthquake)
+100. Al-Adiyat  (The Chargers)
+101. Al-Qari'ah  (The Calamity)
+102. At-Takathur  (The Abundance of Wealth)
+103. Al-Asr  (The Time)
+104. Al-Humazah  (The Scandal-Monger)
+105. Al-Fil  (The Elephant)
+106. Quraysh  (Quraysh)
+107. Al-Ma'un  (Acts of Kindness)
+108. Al-Kawthar  (The Abundance)
+109. Al-Kafirun  (The Disbelievers)
+110. An-Nasr  (The Help)
+111. Al-Lahab  (The Flame)
+112. Al-Ikhlas  (The Sincerity)
+113. Al-Falaq  (The Daybreak)
+114. An-Nas  (Mankind)
 
- handler.help = ['قران']
- handler.tags = ['quran']
- handler.command = ['قران' , 'قرأن' , 'قرآن']
+❀° ┄──•••───╮
+  ♥️ *القرآن الكريم* ♥
+╰───•••──┄ °❀`
+conn.sendFile(m.chat, pp, 'perfil.jpg', lkr, m, false, { mentions: [who] })
+m.react(done)
+}
+handler.help = ['allmenu']
+handler.tags = ['main']
+handler.command = ['سوره', 'سورة'] 
 
- export default handler
+export default handler
