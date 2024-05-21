856f1f4e4a804c778296a4a131a289ed7f24a256
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 18:31:54 +0300

    Update JOHAN-ggg.js

--- plugins/JOHAN-ggg.js
+++ plugins/JOHAN-ggg.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 
 const handler = async (m, {conn, usedPrefix, command}) => {
-  let johan = (هلا انا يوهان)
+  let johan = (هلا انا هيسوكا)
   let Images = (
   conn.sendFile(m.chat,johan , '', , m);
   
