26b09d43f607c32ba1a61c72473347596851bf8a
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 17:35:07 +0300

    Update yts-search.js

--- plugins/yts-search.js
+++ plugins/yts-search.js
@@ -1,7 +1,7 @@
 import yts from 'yt-search'
 import fs from  'fs'
 let handler = async (m, {conn, text }) => {
-  if (!text) throw ' هذا الامر خاص بالبحث في اليوتوب وأخذ رابط الفيديو \n مثلا :\n *.yts*   JOHAN bot'
+  if (!text) throw ' هذا الامر خاص بالبحث في اليوتوب وأخذ رابط الفيديو \n مثلا :\n *.yts*   HISOKA bot'
   await conn.reply(m.chat, global.wait, m)
   let results = await yts(text)
   let tes = results.all
