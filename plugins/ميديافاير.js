c9770f6603920e99670164b15923949fa7d5e65a
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 19:42:32 +0300

    Create ميديافاير.js

---
+++ plugins/ميديافاير.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+import {mediafiredl} from '@bochilteam/scraper';
+
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+  if (!args[0]) throw `_*الامر خاص بالتحميل من ميديافاير />*_\n\n*[ ℹ️ ] ضع الامر ثم ضع رابط الملف الذي تريد تحميله من المهم ان يكون حجمه اقل من 100 ميغا*\n\n*[ 💡 ] مثل:* _${usedPrefix + command} https://www.mediafire.com/file/r0lrc9ir5j3e2fs/DOOM_v13_UNCLONE_`;
+  try {
+    const resEX = await mediafiredl(args[0]);
+    const captionES = `_*جاري إرسال طلبك...*_\n`
+    
+.trim();
+    m.reply(captionES);
+    await conn.sendFile(m.chat, resEX.url, resEX.filename, '', m, null, {mimetype: resEX.ext, asDocument: true});
+  } catch {
+    try {
+      const res = await mediafireDl(args[0]);
+      const {name, size, date, mime, link} = res;
+      const caption = `_* جاري إرسال طلبك...*_ \n`
+
+      .trim();
+      await m.reply(caption);
+      await conn.sendFile(m.chat, link, name, '', m, null, {mimetype: mime, asDocument: true});
+    } catch {
+      await m.reply('_*< ميديافاير />*_\n\n* حدث خطأ.  الرجاء معاودة المحاولة في وقت لاحق.*');
+    }
+  }
+};
+handler.command = /^(mediafire|ميديافاير|m)$/i;
+export default handler;
+
+async function mediafireDl(url) {
+  const res = await axios.get(`https://www-mediafire-com.translate.goog/${url.replace('https://www.mediafire.com/', '')}?_x_tr_sl=en&_x_tr_tl=fr&_x_tr_hl=en&_x_tr_pto=wapp`);
+  const $ = cheerio.load(res.data);
+  const link = $('#downloadButton').attr('href');
+  const name = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ', '').replaceAll('\n', '');
+  const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text();
+  const size = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ', '');
+  let mime = '';
+  const rese = await axios.head(link);
+  mime = rese.headers['content-type'];
+  return {name, size, date, mime, link};
+}
