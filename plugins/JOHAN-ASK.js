6c274962640ad0aed84563bf870a025bec7a7889
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 17:41:05 +0300

    Update JOHAN-ASK.js

--- plugins/JOHAN-ASK.js
+++ plugins/JOHAN-ASK.js
@@ -19,7 +19,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 *❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇*
 *❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇*
-*★صنع بحب من قبل يوهان★*
+*★صنع بحب من قبل هيسوكا★*
 `.trim()
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
