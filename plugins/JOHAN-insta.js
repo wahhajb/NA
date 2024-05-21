5edbe17e5c81c4251892cc62b049e34eb1c3d8ec
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-21 18:39:23 +0300

    Update JOHAN-insta.js

--- plugins/JOHAN-insta.js
+++ plugins/JOHAN-insta.js
@@ -5,18 +5,10 @@ import {instagram} from '@xct007/frieren-scraper';
 import {instagramdl} from '@bochilteam/scraper';
 import instagramDl from '@sasmeee/igdl';
 import {fileTypeFromBuffer} from 'file-type';
-
-
-
 const handler = async (m, {conn, args, command, usedPrefix}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.descargas_instagram
-
-
-  if (!args[0]) throw `${tradutor.texto1} _${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link_`;
-  m.reply(global.wait);
+  if (!args[0]) throw `_*حط رابط من الانستا.*\n\n*[ 🎴 ] مثال:* _${usedPrefix + command} هنا الرابط_`;
+  m.reply(wait);
+  m.react(rwait)
   try {
 const img = await instagramDl(args[0]);
 for (let i = 0; i < img.length; i++) {
@@ -25,46 +17,52 @@ for (let i = 0; i < img.length; i++) {
             await conn.sendMessage(m.chat, {image: {url: img[i].download_link}}, {quoted: m});
         } else if (bufferInfo.detectedType.mime.startsWith('video/')) {
             await conn.sendMessage(m.chat, {video: {url: img[i].download_link }}, {quoted: m});
+          m.react(done)
         }
 }
   } catch {   
   try {
+    let cap = 'Here is your insta video';
     const datTa = await instagram.download(args[0]);
     for (const urRRl of datTa) {
       const shortUrRRl = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-      const tXXxt = `${tradutor.texto2} _${shortUrRRl}_`.trim();
-      conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, m);
+      const tXXxt = `_*< Instagram downloader />*_\n\n▢ *URL:* _${shortUrRRl}_`.trim();
+      conn.sendFile(m.chat, urRRl.url, 'error.mp4', tXXxt, cap, m);
       await new Promise((resolve) => setTimeout(resolve, 10000));
     }
   } catch {
       try {
+        let cap = 'تم تحميل الفيديو';
         const resultss = await instagramGetUrl(args[0]).url_list[0];
         const shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-        const txt2 = `${tradutor.texto2} _${shortUrl2}_`.trim();
-        await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m);
+        const txt2 = `_*< Instagram downloader/>*_\n\n▢ *URL:* _${shortUrl2}_`.trim();
+        await conn.sendFile(m.chat, resultss, 'error.mp4', txt2, cap, m);
       } catch {
         try {
+          let cap = 'Here is your insta video';
           const resultssss = await instagramdl(args[0]);
           const shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-          const txt4 = `${tradutor.texto2} _${shortUrl3}_`.trim();
+          const txt4 = `_*< Instagram downloader/>*_\n\n▢ *URL:* _${shortUrl3}_`.trim();
           for (const {url} of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m);
         } catch {
           try {
+            let cap = 'Here is your insta video';
             const human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`);
             const json = await human.json();
             const videoig = json.result;
             const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text();
-            const txt1 = `${tradutor.texto2} _${shortUrl1}_`.trim();
-            await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m);
+            const txt1 = `_*< Instagram downloader />*_\n\n▢ *URL:* _${shortUrl1}_`.trim();
+            await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, cap, m);
           } catch {
-            throw `${tradutor.texto3}`;
+            throw `_*❌ حصل خطاء في التحميل ، حاول لاحقاً_*`;
           }
         }
       }
     }
   }
 };
-handler.command = /^(انستا|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
+handler.command = /^(انستقرام|انستا|انستغرام|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
+handler.tags = ['downloader'];
 export default handler;
 
 const getBuffer = async (url, options) => {
