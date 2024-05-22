6077d5eeabeeace59ccb03914f167b3c5bdeda9f
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-22 21:27:16 +0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/370f5ae57b2615403c1d5.mp4'; 'https://telegra.ph/file/4b3fc3c942cc6ac2647e9.mp4'
+let videoUrl = ['https://telegra.ph/file/370f5ae57b2615403c1d5.mp4', 'https://telegra.ph/file/4b3fc3c942cc6ac2647e9.mp4']
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
