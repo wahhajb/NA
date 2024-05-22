856f1f75832886281eb459cffd01b0425a72d01a
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-22 07:17:41 +0300

    Update صوت_سورة.js

--- plugins/صوت_سورة.js
+++ plugins/صوت_سورة.js
@@ -1,7 +1,7 @@
 import {alquran} from '@bochilteam/scraper'
 let handler = async (m, { args, usedPrefix, command }) => {
-    if (!(args[0] || args[1])) throw `البحث عن أي آية في القرآن بالصوت والقراءة :\n${usedPrefix + command} 1 2\n\nاذا لم تفهم اي شيء راسلني  هنا \n https://wa.me/+22247072475`
-    if (isNaN(args[0]) || isNaN(args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nhttps://wa.me/+22247072475 `
+    if (!(args[0] || args[1])) throw `البحث عن أي آية في القرآن بالصوت والقراءة :\n${usedPrefix + command} 1 2\n\nاذا لم تفهم اي شيء راسلني  هنا \n https://wa.me/+967701530463`
+    if (isNaN(args[0]) || isNaN(args[1])) throw `contoh:\n${usedPrefix + command} 1 2\n\nhttps://wa.me/+967701530463 `
     let api = await alquran()
     let mes = `
 ${api[args[0] - 1].ayahs[args[1] - 1].text.ar}
