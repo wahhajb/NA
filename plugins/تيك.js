57d24f17ce1edfc1ca606d199193f66560f3c7fd
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-23 19:38:19 +0300

    Update تيك.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/تيك.js
+++ plugins/تيك.js
@@ -1,35 +1,36 @@
-import fg from 'api-dylux' 
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
+import { tiktokdl } from '@bochilteam/scraper';
+import fg from 'api-dylux';
 
-let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!args[0]) throw ` أين هو رابط فيديو التكتوك الذي تود تحميله\n\n 📌 مثال : \n${usedPrefix + command} https://vm.tiktok.com/ZMqysVD/?k=1`
-if (!args[0].match(/tiktok/gi)) throw `*تحقق من ان الرابط من تيك توك*`
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  
+ if (!args[0] && m.quoted && m.quoted.text) {
+  args[0] = m.quoted.text;
+}
+if (!args[0] && !m.quoted) throw `اعطني الرابط \n\nمثال: https://vm.tiktok.com/ZMMPhv9Fb/`;
+ if (!args[0].match(/tiktok/gi)) throw `تأكد من ان الرابط رابط تيك توك`;
+ 
+ 
+  let txt = 'انا لا اتحمل ذنب اغانيك ';
 
-try {
-    let p = await fg.tiktok(args[0]) 
-    let te = `
-┌─⊷ تيگ توگ
-▢ *اسم المستخدم:* ${p.unique_id}
-▢ الوصف:* ${p.title}
-▢ *المده:* ${p.duration}
-└───────────`
-   conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
-    } catch {  	
-	const { author: { nickname }, video, description } = await tiktokdl(args[0])
-         .catch(async _ => await tiktokdlv2(args[0]))
-         .catch(async _ => await tiktokdlv3(args[0]))
-    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-    if (!url) throw '❎ خطأ في تحميل الفيديو '
-     conn.sendFile(m.chat, url, 'fb.mp4', `
-┌─⊷ *تيگ توگ*
-▢ *اسم المستخدم:* ${nickname} ${description ? `\n▢ *الوصف:* ${description}` : ''}
-└───────────`, m)
-} 
+  try {
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
+    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
     
-}  
-handler.help = ['tiktok']
-handler.tags = ['dl']
-handler.command = /^(tiktok|تيكتوك|تيك|tiktoknowm)$/i
-handler.diamond = false
+    if (!url) throw global.error;
+    
+    conn.sendFile(m.chat, url, 'tiktok.mp4', '', m);
+  } catch (err) {
+    try {
+      let p = await fg.tiktok(args[0]);
+      conn.sendFile(m.chat, p.play, 'tiktok.mp4', txt, m);
+    } catch {
+      m.reply('*An unexpected error occurred*');
+    }
+  }
+};
 
-export default handler
+handler.help = ['tiktok'].map((v) => v + ' <url>');
+handler.tags = ['downloader'];
+handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))|تيك|تيكتوك$/i;
+
+export default handler;
