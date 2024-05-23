275297b3039123758cf65fec632d672250942c7b
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-23 11:58:27 +0300

    Update بين.js

--- plugins/بين.js
+++ plugins/بين.js
@@ -1,10 +1,10 @@
 import { pinterest } from '@bochilteam/scraper'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `🐦ماهي الصوره التي تريد البحث عنها؟?\n\n🗿 مثال : ${usedPrefix + command} gojo satura`
+  if (!text) throw `🐦ماهي الصوره التي تريد البحث عنها؟\n\n🗿 مثال : ${usedPrefix + command} gojo satura`
   const json = await pinterest(text)
   conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
-*▢  Pinterest:*  ${text}
+*▢ pinterest:*  ${text}
 `.trim(), m)
 }
 handler.help = ['pinterest']
