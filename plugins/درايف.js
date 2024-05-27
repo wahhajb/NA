880dabff6e6c87bede89ee3618dbf6d9a7adc6f6
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-28 00:13:49 +0300

    Update درايف.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/درايف.js
+++ plugins/درايف.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   }
 }
 handler.help = ['gdrive']
-handler.tags = ['downloader', 'premium']
+handler.tags = ['downloader']
 handler.command = ['gdrive']
 handler.credit = true
 handler.premium = true
