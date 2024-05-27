19a4b78b169acec6a3d9b4e338c96062592cdeb0
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-27 22:29:24 +0300

    Update درايف.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/درايف.js
+++ plugins/درايف.js
@@ -2,22 +2,22 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `✳️ Enter a Google Drive link`
+	if (!args[0]) throw `قم بوضع رابط التحميل من جوجل درايف`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
 	 await m.reply(`
-  *جاري التنزيل...*`)
+*جاري التنزيل*`)
 		
 	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
 	m.react(done)
    } catch {
-	m.reply('Error: Check the link or try another link') 
+	m.reply('خطاء في رابط التحميل حاول مجددا') 
   }
 }
 handler.help = ['gdrive']
 handler.tags = ['downloader', 'premium']
-handler.command = ['gdrive']
+handler.command = ['gdrive', 'درايف']
 handler.credit = true
 handler.premium = true
 
