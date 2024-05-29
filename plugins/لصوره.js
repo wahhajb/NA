7fc055d3f42a2c98ee028708894c2358d55bb339
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-29 12:58:01 +0300

    Update لصوره.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/لصوره.js
+++ plugins/لصوره.js
@@ -1,15 +1,23 @@
-import { webp2png } from '../lib/webp2mp4.js'
-let handler = async (m, { conn, usedPrefix, command }) => {
-const notStickerMessage = `*رد علي الملصق الذي تريد تحويله لصوره ${usedPrefix + command}*`
-if (!m.quoted) throw notStickerMessage
-const q = m.quoted || m
-let mime = q.mediaType || ''
-if (!/sticker/.test(mime)) throw notStickerMessage
-let media = await q.download()
-let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-await conn.sendFile(m.chat, out, 'error.png', null, m)
-}
-handler.help = ['toimg (reply)']
-handler.tags = ['sticker']
-handler.command = ['لصوره', 'صورة', 'لصورة']
-export default handler
+import {webp2png} from '../lib/webp2mp4.js';
+
+
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.convertidor_toimg
+
+
+  const notStickerMessage = `*${tradutor.texto1} ${usedPrefix + command}*`;
+  if (!m.quoted) throw notStickerMessage;
+  const q = m.quoted || m;
+  const mime = q.mediaType || '';
+  if (!/sticker/.test(mime)) throw notStickerMessage;
+  const media = await q.download();
+  const out = await webp2png(media).catch((_) => null) || Buffer.alloc(0);
+  await conn.sendFile(m.chat, out, 'error.png', null, m);
+};
+handler.help = ['toimg (reply)'];
+handler.tags = ['sticker'];
+handler.command = ['لصوره'];
+export default handler;
