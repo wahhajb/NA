c87b7619ac78a859ee89bc3b366e7a377fd8105d
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-29 13:10:33 +0300

    Update لصوت.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/لصوت.js
+++ plugins/لصوت.js
@@ -1,16 +1,29 @@
-import {toAudio} from '../lib/converter.js';
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q || q.msg).mimetype || q.mediaType || '';
-  if (!/video|audio/.test(mime)) throw `*هذا الامر خاص بتحويل مقطع فيديو لموسيقى*`;
-  const media = await q.download();
-  if (!media) throw '*تأكد انك قمت بارسال فيديو وتأكد انك كتبت \n.tomp3*';
-  const audio = await toAudio(media, 'mp4');
-  if (!audio.data) throw '*حذث خطأ راسل صاحب البوت*';
-  conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
-};
-;
-handler.help = ["tomp3"]
-handler.tags = ["tools"]
-handler.command = /^(لصوت)$/i
-export default handler;
+cmd({
+    pattern: "toaudio",
+    alias:['لصوتي','tomp3','لصوت'],
+    desc: "changes type to audio.",
+    category: "converter",
+    use: '<reply to any Video>',
+    filename: __filename
+},
+async(Void, citel, text) => {
+    if (!citel.quoted) return citel.reply(`*رد عـلـى فـيـديـو*`);
+    let mime = citel.quoted.mtype
+if (mime =="audioMessage" || mime =="videoMessage")
+{
+    let media = await Void.downloadAndSaveMediaMessage(citel.quoted);
+     const { toAudio } = require('../lib');
+     let buffer = fs.readFileSync(media);
+    let audio = await toAudio(buffer);
+    Void.sendMessage(citel.chat, { audio: audio, mimetype: 'audio/mpeg' }, { quoted: citel });
+ 
+
+fs.unlink(media, (err) => {
+if (err) { return console.error('File Not Deleted from From TOAUDIO AT : ' , media,'\n while Error : ' , err);  }
+else return console.log('File deleted successfully in TOAUDIO MP3 at : ' , media);
+});
+
+}
+else return citel.reply ("*رد عـلـى فـيـديـو*")
+}
+)
