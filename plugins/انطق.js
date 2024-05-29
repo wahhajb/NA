51a37e260954951e044351b29832cdaa6d22330e
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-29 14:57:29 +0300

    Update انطق.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/انطق.js
+++ plugins/انطق.js
@@ -1,39 +1,45 @@
 import gtts from 'node-gtts'
 import { readFileSync, unlinkSync } from 'fs'
 import { join } from 'path'
-const defaultLang = 'ar'
+
+const defaultLang = 'en'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-let lang = args[0]
-let text = args.slice(1).join(' ')
-if ((args[0] || '').length !== 2) { 
-lang = defaultLang 
-text = args.join(' ')
+
+  let lang = args[0]
+  let text = args.slice(1).join(' ')
+  if ((args[0] || '').length !== 2) {
+    lang = defaultLang
+    text = args.join(' ')
+  }
+  if (!text && m.quoted?.text) text = m.quoted.text
+
+  let res
+  try { res = await tts(text, lang) }
+  catch (e) {
+    m.reply(e + '')
+    text = args.join(' ')
+    if (!text) throw `📌 مثال : \n${usedPrefix}${command} en hello world`
+    res = await tts(text, defaultLang)
+  } finally {
+    if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
+  }
 }
-if (!text && m.quoted?.text) text = m.quoted.text
-let res
-try { res = await tts(text, lang) }
-catch (e) {
-m.reply(e + '')
-text = args.join(' ')
-if (!text) throw `*[❗ملاحظه❗] اكتب نصا والبوت سينطقه,مثال: ${usedPrefix + command} مرحبا*`
-res = await tts(text, defaultLang)
-} finally {
-if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)    
-}}
-handler.help = ['tts <lang> <teks>']
+handler.help = ['tts <lang> <task>']
 handler.tags = ['tools']
-handler.command = /^g?انطق$/i
+handler.command = ['tts', 'انطق'] 
+
 export default handler
 
-function tts(text, lang = 'es') {
-console.log(lang, text)
-return new Promise((resolve, reject) => {
-try {
-let tts = gtts(lang)
-let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
-tts.save(filePath, text, () => {
-resolve(readFileSync(filePath))
-unlinkSync(filePath)
-})
-} catch (e) { reject(e) }
-})}
+function tts(text, lang = 'en-en') {
+  console.log(lang, text)
+  return new Promise((resolve, reject) => {
+    try {
+      let tts = gtts(lang)
+      let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
+      tts.save(filePath, text, () => {
+        resolve(readFileSync(filePath))
+        unlinkSync(filePath)
+      })
+    } catch (e) { reject(e) }
+  })
+}
