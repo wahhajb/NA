831477f6deffb3fea3eed0f6e81d80e4d840503e
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-29 15:11:51 +0300

    Update لصوره.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/لصوره.js
+++ plugins/لصوره.js
@@ -1,32 +1,21 @@
-cmd({
-        pattern: "لصوره",
-        alias : ['لصورة', 'photo'],
-        desc: "Makes photo of replied sticker.",
-        category: "converter",
-        use: '<reply to any gif>',
-        filename: __filename
-    },
-    async(Void, citel, text) => {
-        const getRandom = (ext) => {
-            return `${Math.floor(Math.random() * 10000)}${ext}`
-        }
-        if (!citel.quoted) return citel.reply(`*رد على ملصق لتحويله الى صوره*`)
-        let mime = citel.quoted.mtype
-if (mime =="imageMessage" || mime =="stickerMessage")
-{
-        let media = await Void.downloadAndSaveMediaMessage(citel.quoted);
-        let name = await getRandom('.png')
-        exec(`ffmpeg -i ${media} ${name}`, (err) => {
-            let buffer = fs.readFileSync(media)
-            Void.sendMessage(citel.chat, { image: buffer }, { quoted: citel })
-          
-         fs.unlink(media, (err) => {
-         if (err) { return console.error('File Not Deleted from From TOPHOTO AT : ' , media,'\n while Error : ' , err);  }
-         else return console.log('File deleted successfully in TOPHOTO  at : ' , media);
-         });
-         
-        })
-        
-} else return citel.reply ("*رد على ملصق لتحويله الى صوره*")
-    }
-)
+import { webp2png } from '../lib/webp2mp4.js'
+
+var handler = async (m, { conn, usedPrefix, command }) => {
+
+const notStickerMessage = `*⚠️ الرد بالامر ${usedPrefix + command} الى الملصق المراد تحويله الى صوره*`
+if (!m.quoted) throw notStickerMessage 
+const q = m.quoted || m
+let mime = q.mediaType || ''
+if (!/sticker/.test(mime)) throw notStickerMessage
+let media = await q.download()
+let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
+await conn.sendFile(m.chat, out, 'error.png', null, fkontak, m)
+
+}
+handler.help = ['toimg']
+handler.tags = ['transformador']
+handler.command = ['لصوره']
+
+handler.limit = true
+
+export default handler
