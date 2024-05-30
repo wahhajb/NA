1c843df6fd05321a64adb2570ff81d7e77b2a938
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-30 13:52:12 +0300

    Update لصوره.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/لصوره.js
+++ plugins/لصوره.js
@@ -1,21 +1,44 @@
-import { webp2png } from '../lib/webp2mp4.js'
+ import sharp from 'sharp'
 
-var handler = async (m, { conn, usedPrefix, command }) => {
+ const TIMEOUT = 10000; // 10 detik
 
-const notStickerMessage = `*⚠️ RESPONDA CON ${usedPrefix + command} AL STICKER QUE DESEE CONVERTIR EN IMAGEN*`
-if (!m.quoted) throw notStickerMessage 
-const q = m.quoted || m
-let mime = q.mediaType || ''
-if (!/sticker/.test(mime)) throw notStickerMessage
-let media = await q.download()
-let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-await conn.sendFile(m.chat, out, 'error.png', null, fkontak, m)
+ let handler = async (m, { conn, usedPrefix, command }) => {
+   const notStickerMessage = `المرجو الاشارة للملصق الذي تريد ان تحوله لصورة ثم تكتب \n *${usedPrefix + command}*`;
 
-}
-handler.help = ['toimg']
-handler.tags = ['transformador']
-handler.command = ['toimg']
+   if (!m.quoted) throw notStickerMessage;
 
-handler.limit = true
+   const q = m.quoted || m;
+   const mime = q.mimetype || '';
 
-export default handler
+   if (!/image\/webp/.test(mime)) throw notStickerMessage;
+
+   try {
+     // Download sticker
+     const media = await q.download();
+
+     // Dekoding WebP tanpa webp-js
+     const decodedBuffer = await sharp(media).toFormat('png').toBuffer();
+
+     // Send PNG image
+     if (decodedBuffer.length > 0) {
+       await conn.sendFile(m.chat, decodedBuffer, 'out.png', 'instagram.com/noureddine_ouafy', m);
+     } else {
+       throw 'فشل تحويل الملصق إلى صورة.';
+     }
+   } catch (error) {
+     console.error(error);
+     if (error.message === 'Timeout of 10000ms exceeded') {
+       m.reply('فشلت عملية التحويل.');
+     } else {
+       m.reply(`فشلت عملية التحويل`);
+     }
+   }
+ };
+
+ handler.help = ['toimage']
+ handler.tags = ['sticker']
+ handler.command = ['toimage']
+
+ 
+
+ export default handler
