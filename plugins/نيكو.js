d860983b993eff918dab2f4b3a8962215b41fab2
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-30 22:44:15 +0300

    Update نيكو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/نيكو.js
+++ plugins/نيكو.js
@@ -1,16 +1,14 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 let ne = await (await fetch('https://raw.githubusercontent.com/ArugaZ/grabbed-results/main/random/anime/neko.txt')).text()
-try {
 let nek = ne.split('\n')
 let neko = pickRandom(nek)
-conn.sendFile(m.chat, neko, 'error.jpg', 'Nyaww~ 🐾💗', m, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: 'Nyaww~ 🐾💗', body: '𝐒𝐮𝐩𝐞𝐫 𝐁𝐨𝐭 𝐃𝐞 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩', previewType: 1, thumbnail: imagen4, sourceUrl: [nna, md, yt, tiktok, fb].getRandom()}}})
-} catch (e) {
-console.log(e)}}
+//conn.sendFile(m.chat, neko, 'error.jpg', `Nyaww~ 🐾💗`, m)
+conn.sendButton(m.chat, 'Nyaww~ 🐾💗', wm, neko, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], null, null, m)
+}
 handler.command = /^(neko)$/i
 handler.tags = ['anime']
 handler.help = ['neko']
-handler.register = true
 export default handler
 function pickRandom(list) {
 return list[Math.floor(Math.random() * list.length)]
