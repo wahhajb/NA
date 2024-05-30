9ef375e0a8fd6f0a47a280f43dde421b5a8b30cc
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-30 13:56:52 +0300

    Update لفيديو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/لفيديو.js
+++ plugins/لفيديو.js
@@ -1,25 +1,45 @@
-import { webp2mp4 } from '../lib/webp2mp4.js'
+import fetch from 'node-fetch'
+import { sticker } from '../lib/sticker.js'
+import uploadFile from '../lib/uploadFile.js'
+import uploadImage from '../lib/uploadImage.js'
+import { webp2png, webp2mp4} from '../lib/webp2mp4.js'
+import { Sticker, StickerTypes } from 'wa-sticker-formatter'
 import { ffmpeg } from '../lib/converter.js'
-let handler = async (m, { conn, usedPrefix, command }) => {
-if (!m.quoted) throw `*اعمل ريبلاي للاستيكر ال عاوز تحوله لفديو يحب ${usedPrefix + command}*`
-let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw `*اعمل ريبلاي للاستيكر ال عاوز تحوله لفديو يحب ${usedPrefix + command}*`
-let media = await m.quoted.download()
-let out = Buffer.alloc(0)
-if (/webp/.test(mime)) {
-out = await webp2mp4(media)
-} else if (/audio/.test(mime)) {
-out = await ffmpeg(media, [
-'-filter_complex', 'color',
-'-pix_fmt', 'yuv420p',
-'-crf', '51',
-'-c:a', 'copy',
-'-shortest'
-], 'mp3', 'mp4')
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+let stiker = false
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (!m.quoted) throw `المرجو الاشارة للملصق المتحرك الذي تريد تحويله لفيديو`
+
+    let img = await q.download?.()
+    let stek = new Sticker(img, { pack: packname, author: author, type: StickerTypes.FULL })
+    let buffer = await stek.toBuffer()
+    let out
+      try {
+        if (/webp/g.test(mime)) out = await webp2mp4(img)
+        else if (/image/g.test(mime)) out = await uploadImage(img)
+        else if (/video/g.test(mime)) out = await uploadFile(img)
+        else if (/audio/g.test(mime)) out = await ffmpeg(media, [
+            '-filter_complex', 'color',
+            '-pix_fmt', 'yuv420p',
+            '-crf', '51',
+            '-c:a', 'copy',
+            '-shortest'
+        ], 'mp3', 'mp4')
+        if (typeof out !== 'string') out = await uploadImage(img)
+        else if (/gif/g.test(mime)) out = stek
+      } catch (e) {
+        throw eror
+      }
+     await conn.sendFile(m.chat, out, 'tovid.mp4', 'تم عملية تحويل الملصق لفيديو بنجاح \n تابعني في الانستغرام\ninstagram.com/noureddine_ouafy' , m)
 }
-await conn.sendFile(m.chat, out, 'error.mp4', '*تم*', m, 0, { thumbnail: out })
-}
+//lo mau apa??
 handler.help = ['tovideo']
 handler.tags = ['sticker']
-handler.command = ['لفديو', 'tomp4', 'لمقطع', 'لفيديو']
+handler.command = /^tovideo$/i
+
 export default handler
