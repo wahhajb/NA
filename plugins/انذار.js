037ddfff2e070ac9d7f7aae700cb60db2c970eae
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-05-31 17:57:29 +0300

    Update انذار.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/انذار.js
+++ plugins/انذار.js
@@ -1,39 +1,39 @@
+
 let war = global.maxwarn
 let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }) => {      
-  let who
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
-  else who = m.chat
-  if (!who) throw `✳️ قم بعمل تاغ للشخص المراد تحذيره\n\n📌 مثال: ${usedPrefix + command} @user`
-  if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة بياناتي`
-  let name = conn.getName(m.sender)
-  let warn = global.db.data.users[who].warn
-  if (warn < war) {
-    global.db.data.users[who].warn += 1
-    m.reply(`
-⚠️ *انذار للمستخدم* ⚠️
+        let who
+        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+        else who = m.chat
+        if (!who) throw `✳️ Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @user`
+        if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+        let name = conn.getName(m.sender)
+        let warn = global.db.data.users[who].warn
+        if (warn < war) {
+            global.db.data.users[who].warn += 1
+            m.reply(`
+⚠️ *Warned User* ⚠️
 
-▢ *المشرف:* ${name}
-▢ *المستخدم:* @${who.split`@`[0]}
-▢ *الانذارات:* ${warn + 1}/${war}
-▢ *السبب:* ${text}`, null, { mentions: [who] }) 
-    m.reply(`
-⚠️ *انذار* ⚠️
-لقد تلقيت انذار من أحد المشرفين
+▢ *Admin:* ${name}
+▢ *User:* @${who.split`@`[0]}
+▢ *Warns:* ${warn + 1}/${war}
+▢ *Reason:* ${text}`, null, { mentions: [who] }) 
+            m.reply(`
+⚠️ *caution* ⚠️
+You received a warning from an admin
 
-▢ *الانذارات:* ${warn + 1}/${war} 
-إذا تلقيت *${war}* انذارات سيتم طردك تلقائيًا من المجموعة`, who)
-  } else if (warn == war) {
-    global.db.data.users[who].warn = 0
-    m.reply(`⛔ تجاوز المستخدم عدد الانذارات المسموح بها *${war}*، سيتم طرده`)
-    await time(3000)
-    await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
-    m.reply(`♻️ لقد تمت إزالتك من المجموعة *${groupMetadata.subject}* بسبب تلقيك *${war}* انذارات`, who)
-  }
+▢ *Warns:* ${warn + 1}/${war} 
+if you receive *${war}* warnings you will be automatically removed from the group`, who)
+        } else if (warn == war) {
+            global.db.data.users[who].warn = 0
+            m.reply(`⛔ The user exceeded the *${war}* warnings will therefore be removed`)
+            await time(3000)
+            await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
+            m.reply(`♻️ You were removed from the group *${groupMetadata.subject}* because you have been warned *${war}* times`, who)
+        }
 }
-
 handler.help = ['warn @user']
 handler.tags = ['group']
-handler.command = ['warn','انذار'] 
+handler.command = ['warn'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
@@ -41,5 +41,5 @@ handler.botAdmin = true
 export default handler
 
 const time = async (ms) => {
-  return new Promise(resolve => setTimeout(resolve, ms));
-                                             }
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
