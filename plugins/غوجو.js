7a3804cbc030c659be06a0ae8ec73e743b045709
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-03 17:24:55 +0300

    Create غوجو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

---
+++ plugins/غوجو.js
@@ -0,0 +1,17 @@
+const dir = [
+'https://telegra.ph/file/c1094944f7262a3fc5e05.mp4',
+'https://telegra.ph/file/c75e2105df6f9e06c99fc.mp4',
+'https://telegra.ph/file/c60ce32660fb039790338.mp4',
+'https://telegra.ph/file/5380039619f266674afda.mp4',
+'https://telegra.ph/file/260eaa48d793ca0ac43df.mp4',
+'https://telegra.ph/file/ff77e44ebb37104712ffb.mp4'
+];
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
+  await conn.sendMessage(m.chat, { react: { text: '📹', key: m.key } })
+}
+handler.help = ['dado']
+handler.tags = ['game']
+handler.command = ['كيلوا'] 
+handler.limite = true 
+export default handler
