38c0843b498e21e14299f05f8fbc6ba156000d11
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-04 02:51:01 +0300

    Update بريم.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/بريم.js
+++ plugins/بريم.js
@@ -1,16 +1,29 @@
-let handler = async (m, { conn, text }) => {
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 let who
-if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-else who = m.chat
-if (!who) throw `*[❗]   نسيت المنشن @منشن ارجو من اختيار احد الاعضاء لجعلهم مميزين*`
-if (global.prems.includes(who.split`@`[0])) throw '*[❗] المستخدم الذي تم إدخاله هو بالفعل مستخدم متميز (بريميوم)*'
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = global.db.data.users[who]
+    if (!who) throw `✳️ منشن على شخص\n\n📌 مثال : ${usedPrefix + command} @user`
+if (global.prems.includes(who.split`@`[0])) throw '✳️ لديه العضويه المميزه من قبل'
 global.prems.push(`${who.split`@`[0]}`)
-let textprem = `*[❗] @${who.split`@`[0]} أنت الآن مستخدم متميز ولن يكون لديك حدود عند استخدام البوت (تستطيع استخدام اي امر مثل المطور)*`
-m.reply(textprem, null, { mentions: conn.parseMention(textprem) })
+
+conn.reply(m.chat, `
+✅ بريمو
+
+@${who.split`@`[0]} now you become a premium user
+┌───────────
+▢ *الرقم:* ${user.name}
+└───────────
+`, m, { mentions: [who] })
+
 }
-handler.help = ['addprem <@user>']
+handler.help = ['addprem <@tag>']
 handler.tags = ['owner']
-handler.command = /^اضف-بريم$/i
+handler.command = ['addprem', 'اضف-بريم'] 
+
 handler.group = true
 handler.rowner = true
+
 export default handler
