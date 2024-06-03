5fa22f03c1ec804ab15d6547dbd392393dd28be3
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-04 01:30:58 +0300

    Update وايفو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/وايفو.js
+++ plugins/وايفو.js
@@ -1,20 +1,50 @@
 import fetch from 'node-fetch';
 
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  m.react(rwait);
 
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
-  const tradutor = _translate.plugins.random_waifu
+  let type = (command).toLowerCase();
+  let baseUrl = 'https://weeb-api.vercel.app/';
 
-  const res = await fetch('https://api.waifu.pics/sfw/waifu');
-  if (!res.ok) throw await res.text();
-  const json = await res.json();
-  if (!json.url) throw 'Error!';
-  conn.sendFile(m.chat, json.url, 'error.jpg', tradutor.texto1, m);
-// conn.sendButton(m.chat, `𝙰-𝙰𝚁𝙰 𝙰𝚁𝙰 𝚂𝙴𝙼𝙿𝙰𝙸~~`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+  const fetchImage = async (endpoint) => {
+    try {
+      const response = await fetch(baseUrl + endpoint);
+      if (!response.ok) throw `❎ حدث خطاء اثناء حلب ${type} صوره`;
+      const imageBuffer = await response.buffer(); // Get the image data as a buffer
+      conn.sendFile(m.chat, imageBuffer, 'img.jpg', `✅ تم ${type}`, m);
+      m.react(dmoji);
+    } catch (error) {
+      console.error(error);
+      m.reply(`❎ حدث خطاء اثناء جلب الملف  ${type} صوره.`);
+    }
+  };
+
+  switch (type) {
+    case 'loli':
+      fetchImage('loli');
+      break;
+
+    case 'waifu':
+      fetchImage('waifu');
+      break;
+
+    case 'neko':
+      fetchImage('neko');
+      break;
+
+    case 'zerotwo':
+      fetchImage('zerotwo');
+      break;
+
+    default:
+      
+      break;
+  }
 };
-handler.help = ['waifu'];
-handler.tags = ['anime'];
-handler.command = /^(وايفو|waifu)$/i;
-export default handler;
+
+handler.help = ['waifu', 'neko', 'zerotwo', 'loli']
+handler.tags = ['anime']
+handler.command = ['وايفو', 'نيكو', 'زوروتو', 'لولي'] 
+
+
+export default handler
