36d9fdd0580ce573b93f47a0d2452914a7a78d4e
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-04 00:39:56 +0300

    Update غوجو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/غوجو.js
+++ plugins/غوجو.js
@@ -4,7 +4,11 @@ const dir = [
 'https://telegra.ph/file/c60ce32660fb039790338.mp4',
 'https://telegra.ph/file/5380039619f266674afda.mp4',
 'https://telegra.ph/file/260eaa48d793ca0ac43df.mp4',
-'https://telegra.ph/file/ff77e44ebb37104712ffb.mp4'
+'https://telegra.ph/file/ff77e44ebb37104712ffb.mp4',
+'https://telegra.ph/file/c3530a6237e743e815752.mp4',
+'https://telegra.ph/file/ff7672048f51cfc606cb8.mp4',
+'https://telegra.ph/file/f616865c9390191a7911a.mp4',
+'https://telegra.ph/file/517702ef07ddd9663e63c.mp4',
 ];
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
