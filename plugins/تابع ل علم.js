70f516965fb2a589519a87e231f93508bd9f3284
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-04 02:46:54 +0300

    Update تابع ل علم.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/تابع ل علم.js
+++ plugins/تابع ل علم.js
@@ -6,25 +6,25 @@ export async function before(m) {
         return !0
     this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
     if (!(id in this.tebakbendera))
-        return this.reply(m.chat, 'The game was over', m)
+        return this.reply(m.chat, 'انتهت اللعبه', m)
     if (m.quoted.id == this.tebakbendera[id][0].id) {
         let isSurrender = /^((me)?nyerah|surr?ender)$/i.test(m.text)
         if (isSurrender) {
             clearTimeout(this.tebakbendera[id][3])
             delete this.tebakbendera[id]
-            return this.reply(m.chat, '*Yeah, give up :( !*', m)
+            return this.reply(m.chat, '*نعم, استسلم :( !*', m)
         }
         let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
         
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
-            this.reply(m.chat, `✅ *correct!*\n+${this.tebakbendera[id][2]} XP`, m)
+            this.reply(m.chat, `✅ *صح!*\n+${this.tebakbendera[id][2]} XP`, m)
             clearTimeout(this.tebakbendera[id][3])
             delete this.tebakbendera[id]
         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
-            m.reply(`❗ *A Little More!*`)
+            m.reply(`❗ *اقتربت من الاجابه!*`)
         else
-            this.reply(m.chat, `❌ *Wrong!*`, m)
+            this.reply(m.chat, `❌ *خطاء!*`, m)
     }
     return !0
 }
