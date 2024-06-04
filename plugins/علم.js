597d3d37b0d51e443490be42902f3e1c46ad60aa
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-05 01:07:33 +0300

    Update علم.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/علم.js
+++ plugins/علم.js
@@ -1,25 +1,25 @@
 import fetch from 'node-fetch'
-let timeout = 120000
+let timeout = 60000
 let poin = 4999
 let handler = async (m, { conn, command, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
     if (id in conn.tebakbendera) {
-        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.tebakbendera[id][0])
+        conn.reply(m.chat, 'هنالك سؤال لم تتم الاجابه عليه', conn.tebakbendera[id][0])
         throw false
     }
     let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakbendera2.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*${command.toUpperCase()}* يرجاء الاجابه باللغه الانجليزيه
-الوقت *${(timeout / 1000).toFixed(2)} ثانيه*
-استخدم ${usedPrefix} تلميح للتلميح
-الجائزة: ${poin} XP
+    let caption = `*${command.toUpperCase()}*
+الوقت *${(timeout / 1000).toFixed(2)} second*
+اكتب ${usedPrefix}fhint للانسحاب
+الجائزه: ${poin} XP
     `.trim()
     conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tebakbendera[id]) conn.reply(m.chat, `انتهى الوقت!\n الاجابه هي *${json.name}*`, conn.tebakbendera[id][0])
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `Time's up!\nThe answer is *${json.name}*`, conn.tebakbendera[id][0])
             delete conn.tebakbendera[id]
         }, timeout)
     ]
