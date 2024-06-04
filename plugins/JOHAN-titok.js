0f3e1fb0016c8a6d9956663b3bb271edd8c40fc2
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-04 15:52:38 +0300

    Update JOHAN-titok.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/JOHAN-titok.js
+++ plugins/JOHAN-titok.js
@@ -1,77 +1,39 @@
-import fg from 'api-dylux';
-import axios from 'axios';
-import cheerio from 'cheerio';
-import {tiktok} from '@xct007/frieren-scraper';
-import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
-import {tiktokdl} from '@bochilteam/scraper';
-const CFROSAPI = global.APIs.CFROSAPI;
-const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-  if (!text) throw `_*تيك توك*_\n\n*_Past a tiktok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
-  if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `_*PRINCE TIKTOK DL*_\n\n*_Enter a TikTok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
-  
-  // let buttons = [{ buttonText: { displayText: '♫ 𝙰𝚄𝙳𝙸𝙾 ♫' }, buttonId: `${usedPrefix}tomp3` }]
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+  if (!text) throw `*يجب عليك إعطاء رابط أي فيديو أو صورة من TikTok*`;
+  m.reply('*الرجاء الانتظار...*');
+
   try {
-    m.react(rwait)
-    const dataFn = await conn.getFile(`${CFROSAPI}/api/tiktokv2?url=${args[0]}`);
-    const desc1n = `*_Here is your requested video_*`;
-    await conn.sendMessage(m.chat, {video: dataFn.data, caption: desc1n}, {quoted: m});
- m.react(done)
-  } catch (ee1) {
-    
-  try {
-    //const aa = {quoted: m, userJid: conn.user.jid};
-    //const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: '𝘗𝘙𝘐𝘕𝘊𝘌-𝘉𝘖𝘛-𝘔𝘋', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/PRINCE-GDS/THE-PRINCE-BOT'}, mentionedJid: [m.sender]}}}, aa);
-    //await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
-    const dataF = await tiktok.v1(args[0]);
-    // let desc1 =  `*كنيه:* ${dataF.nickname || 'Indefinido'}`
-    const desc1 = `*_تم التحميل_*`;
-    await conn.sendMessage(m.chat, {video: {url: dataF.play}, caption: desc1}, {quoted: m});
-  m.react(done)
-  } catch (e1) {
-    try {
-      const tTiktok = await tiktokdlF(args[0]);
-      // let desc2 = `🔗 *الرابط:* ${tTiktok.video}`
-      const desc2 = `*_تم التحميل_*`;
-      await conn.sendMessage(m.chat, {video: {url: tTiktok.video}, caption: desc2}, {quoted: m});
-    m.react(done)
-    } catch (e2) {
-      try {
-        const p = await fg.tiktok(args[0]);
-        // let te = `*الاسم:* ${p.author || 'Indefinido'}`
-        const te = `*_تم التحميل_*`;
-        await conn.sendMessage(m.chat, {video: {url: p.nowm}, caption: te}, {quoted: m});
-    m.react(done)
-      } catch (e3) {
-        try {
-          const {author: {nickname}, video, description} = await tiktokdl(args[0]);
-          const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
-          // let cap = `*كنيه:* ${nickname || 'Indefinido'}`
-          const cap = `*_تم التحميل_*`;
-          await conn.sendMessage(m.chat, {video: {url: url}, caption: cap}, {quoted: m});
-      m.react(done)
-        } catch {
-          throw `*_حصل خطاء يرجى المحاوله لاحقاً._*`;
-          }
-        }
-      }
-    }
+    let mediaURL = await zoro(text);
+
+    if (!mediaURL) throw 'لم يتم العثور على فيديو للرابط المعطى';
+
+    conn.sendFile(m.chat, mediaURL, '', 'هذا هو الفيديو ⚡', m, false, { mimetype: 'video/mp4' });
+  } catch (error) {
+    throw `حدث خطأ: ${error.message}`;
   }
 };
-handler.command = /^(تيك-توك|tk|ttdl|tiktokdl|tiktoknowm|ttnowm|tiktokaudio)$/i;
-handler.tags = ['downloader'];
-export default handler;
 
-async function tiktokdlF(url) {
-  if (!/tiktok/.test(url)) return `_*تيك توك*_\n\n*_Past a tiktok link._*\n\n*_Example:_* _${usedPrefix + command} Url here_`;
-  const gettoken = await axios.get('https://tikdown.org/id');
-  const $ = cheerio.load(gettoken.data);
-  const token = $('#download-form > input[type=hidden]:nth-child(2)').attr( 'value' );
-  const param = {url: url, _token: token};
-  const {data} = await axios.request('https://tikdown.org/getAjax?', {method: 'post', data: new URLSearchParams(Object.entries(param)), headers: {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'}});
-  const getdata = cheerio.load(data.html);
-  if (data.status) {
-    return {status: true, thumbnail: getdata('img').attr('src'), video: getdata('div.download-links > div:nth-child(1) > a').attr('href'), audio: getdata('div.download-links > div:nth-child(2) > a').attr('href')};
-  } else {
-    return {status: false};
-  }
-      }
+async function zoro(text) {
+  let res = await fetch(`https://api-me-4ef1b6491458.herokuapp.com/api/tiktok?url=${encodeURIComponent(text)}`);
+  if (!res.ok) return false;
+
+  const fileName = 'Hisoka_tiktok_video.mp4';
+  const fileStream = fs.createWriteStream(fileName);
+  res.body.pipe(fileStream);
+
+  await new Promise((resolve, reject) => {
+    fileStream.on('finish', resolve);
+    fileStream.on('error', reject);
+  });
+
+  return fileName;
+}
+
+handler.help = ['tiktok'];
+handler.tags = ['downloader'];
+handler.command = /^(تيكتوك|تيك)$/i;
+
+export default handler;
