995fbdba3cf364bdbbe856a59e428b3b6e60adf4
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-06 06:09:14 +0300

    Update كيلوا.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/كيلوا.js
+++ plugins/كيلوا.js
@@ -3,6 +3,10 @@ const dir = [
 'https://telegra.ph/file/8ea6ba7fc0306abba3cba.mp4',
 'https://telegra.ph/file/f369f6a11c43b9e1fb8ce.mp4',
 'https://telegra.ph/file/d1a4ca2020522a0f27c89.mp4',
+'https://telegra.ph/file/c601b4bf77e9aa08f05d8.mp4',
+'https://telegra.ph/file/daeb4d36000407b0d8961.mp4',
+'https://telegra.ph/file/092c5f6f97eab1de4670c.mp4',
+'https://telegra.ph/file/58d689801b25e330eb1e2.mp4',
 ];
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, dir[Math.floor(Math.random() * dir.length)], 'dado.webp', '', m)
