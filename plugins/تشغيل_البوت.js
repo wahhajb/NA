b953590b5855b07978c43d68d4d425cf4abad201
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-07 23:38:19 +0300

    Create تشغيل_البوت.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

---
+++ plugins/تشغيل_البوت.js
@@ -0,0 +1,17 @@
+const handler = async (m) => {
+    global.db.data.chats[m.chat].isBanned = false;
+    global.db.data.chats[m.chat].welcome = true;
+    global.db.data.chats[m.chat].detect = true;
+    global.db.data.chats[m.chat].modohorny = true;
+    global.db.data.chats[m.chat].server = true;
+    global.db.data.chats[m.chat].session = true;
+   
+    global.db.data.chats[m.chat].audios = true;
+    m.reply('*مرحبا ياصديقي*\n*انا بوت هنري*\n*تم تشغيل البوت في هذه الدردشه بأمر من المطور😀*\n*يمكنك الان استعمال البوت في الدردشه الخاصه للبوت*\n*✦━━━━━[ هنري ]━━━━━✦*');
+
+    };
+    handler.help = ['banchat'];
+    handler.tags = ['owner'];
+    handler.command = /تشغيل$/i;
+    handler.rowner = true;
+    export default handler;
