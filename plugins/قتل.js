a01ebc7ec04f3d1592f92323693e79a8d4623308
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-08 22:00:16 +0300

    Create قتل.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

---
+++ plugins/قتل.js
@@ -0,0 +1,16 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*${toM(a)}, 😞هذا الشخص قتل*
+*${toM(b)}, قتل ذا اليوم 😞*\n\*😓الكل يبلغ الشرطة على هذه الجريمه*`, null, {
+mentions: [a, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['formarpareja','قتل']
+handler.group = true
+export default handler
