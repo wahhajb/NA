1166debb9631d285fb348bf6d7ce657e23e074bf
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-08 12:09:12 +0300

    Update حيوان.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/حيوان.js
+++ plugins/حيوان.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn}) => {
 try {
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-let res = await fetch('https://api.waifu.pics/sfw/pat')
+let res = await fetch('https://api.waifu.pics/sfw/dog')
 let json = await res.json()
 let { url } = json
 let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
