49ab823e7c6f7c4807653d2c773cf860694c82c8
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-08 16:26:16 +0300

    Update JOHAN-bot2.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/JOHAN-bot2.js
+++ plugins/JOHAN-bot2.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `تقصد نفسك`;
+    let message = `*هذه نهايتك*`;
     await conn.sendMessage(m.chat, {
    react: {
  text: "💀",
@@ -10,7 +10,7 @@ let handler = async (m, { conn }) => {
    }
   })
 
-    conn.sendFile(m.chat, 'https://telegra.ph/file/caafdb2292db8180b99fe.mp4', 'video.mp4' , message, m);
+    conn.sendFile(m.chat, 'https://telegra.ph/file/af5da33940f19be1f8b38.mp4', 'video.mp4' , message, m);
 };
 
 handler.customPrefix = /^(bot|بوت)$/i;
