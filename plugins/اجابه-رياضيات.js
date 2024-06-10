b5a4a114c8509806689b984ca36f7f0faa23d307
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-10 06:41:12 +0300

    Update اجابه-رياضيات.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/اجابه-رياضيات.js
+++ plugins/اجابه-رياضيات.js
@@ -4,22 +4,22 @@ let handler = m => m
 handler.before = async function (m) {
     if (!/^-?[0-9]+(\.[0-9]+)?$/.test(m.text)) return !0
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.text || !/^▢ HOW MUCH IS IT/i.test(m.quoted.text)) return !0
+    if (!m.quoted || !m.quoted.fromMe || !m.text || !/^▢ حل المسئله الاتيه/i.test(m.quoted.text)) return !0
     this.math = this.math ? this.math : {}
-    if (!(id in this.math)) return this.reply(m.chat, 'The game is over', m)
+    if (!(id in this.math)) return this.reply(m.chat, 'انتهت المسئله', m)
     if (m.quoted.id == this.math[id][0].id) {
         let math = JSON.parse(JSON.stringify(this.math[id][1]))
         if (m.text == math.result) {
             global.db.data.users[m.sender].exp += math.bonus
             clearTimeout(this.math[id][3])
             delete this.math[id]
-            m.reply(`✅ *Correct answer!*\n\n‣ won : *+${math.bonus} XP*`)
+            m.reply(`✅ *اجابه صحيحه!*\n\nربحت : *+${math.bonus} XP*`)
         } else {
             if (--this.math[id][2] == 0) {
                 clearTimeout(this.math[id][3])
                 delete this.math[id]
-                m.reply(`*The opportunities are over*\n\n Response : *${math.result}*`)
-      } else m.reply(`❎ *Wrong answer*\n\nThere are still  ${this.math[id][2]} opportuniities`)
+                m.reply(`*انتهت فرصك*\n\nالاجابه : *${math.result}*`)
+      } else m.reply(`❎ *غلط*\n\nلسه فيه  ${this.math[id][2]} فرص`)
         }
     }
     return !0
