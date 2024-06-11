57f645df2381f1d2ef1a23107a9de72be0c1da93
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-11 09:17:48 +0300

    Create خلفيه-جروب.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

---
+++ plugins/خلفيه-جروب.js
@@ -0,0 +1,14 @@
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mediaType || ''
+    if (/image/.test(mime)) {
+    let img = await q.download()
+    if (!img) throw '*رد على الصوره لجعلها بروفايل للحروب.*'
+    await conn.updateProfilePicture(m.chat, img).then(_ => m.reply('*تم تغيير بروفايل الحروب ✅*'))
+    } else throw '*رد على الصوره لجعلها بروفايل للجروب.*'}
+    handler.command = /^خلفيه$/i
+    handler.group = true
+    handler.admin = true
+    handler.botAdmin = true
+    export default handler
