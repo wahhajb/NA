0670af4de01f857863c031cd88949951b4ef0c66
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-17 18:17:56 +0300

    Update حيوان.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/حيوان.js
+++ plugins/حيوان.js
@@ -1,15 +1,16 @@
-import { sticker } from '../lib/sticker.js'
-import fetch from 'node-fetch'
-import MessageType from '@adiwajshing/baileys'
-let handler = async (m, { conn}) => {
-try {
-if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
-if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-let res = await fetch('https://api.waifu.pics/sfw/dog')
-let json = await res.json()
-let { url } = json
-let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
-} catch (e) { }}
-handler.command = /^(pat|palmaditas|حيوان|mimos|patt)$/i
-export default handler
+import fetch from 'node-fetch';
+const handler = async (m, {conn, text}) => {
+  try {
+    const res = await fetch('https://api.thedogapi.com/v1/images/search');
+    const img = await res.json();
+    const caption = `🦮`.trim();
+    conn.sendFile(m.chat, img[0].url, 'dog.jpg', caption, m);
+  } catch {
+    throw '*Error!*';
+  }
+};
+handler.help = ['dog'];
+handler.tags = ['random'];
+handler.command = /^كلب$/i;
+handler.fail = null;
+export default handler;
