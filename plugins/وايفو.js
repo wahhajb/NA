f94e4a6688bd5c4ee45ff58dc6695d2da7cb94cb
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-17 11:14:35 +0300

    Update and rename وايفو-تجربات.js to وايفو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/وايفو.js
+++ plugins/وايفو.js
@@ -12,9 +12,9 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   const json = await res.json();
   if (!json.url) throw 'Error!';
   conn.sendFile(m.chat, json.url, 'error.jpg', tradutor.texto1, m);
-// conn.sendButton(m.chat, `𝙰-𝙰𝚁𝙰 𝙰𝚁𝙰 𝚂𝙴𝙼𝙿𝙰𝙸~~`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
+// conn.sendButton(m.chat, `🥵❤️`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 };
 handler.help = ['waifu'];
 handler.tags = ['anime'];
-handler.command = /^(waifu)$/i;
+handler.command = /^(وايفو)$/i;
 export default handler;
