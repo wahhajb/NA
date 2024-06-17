f992c0dc6d397552636b140753f8f870d8644ff3
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-17 22:09:36 +0300

    Update درايف.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/درايف.js
+++ plugins/درايف.js
@@ -1,33 +1,50 @@
-import { GDriveDl } from '../lib/scrape.js'
+import fetch from 'node-fetch';
+import {sizeFormatter} from 'human-readable';
 
-let handler = async (m, { conn, args }) => {
-  if (!(args[0] || '').match(/([\w-]){33}|([\w-]){19}/)) throw '*التحميل من منصة غوغل درايف يكفي ان تكتب الامر متبوع برابط من المنصة مثال :*\n\n*.gdrive2* https://drive.google.com/u/0/uc?id=1-XxkNNYHEvKlbPMEMNbgEBg6tgKEC8bO&export=download'
 
-  const someincludes = ( data, id ) => {
-        let res = data.find(el => id.includes(el) )
-        return res ? true : false;
-    }
 
+const formatSize = sizeFormatter({
+  std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B`});
+
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const tradutor = _translate.plugins.descargas_gdrive
+
+  if (!args[0]) throw `${tradutor.texto1} _${usedPrefix + command} https://drive.google.com/file/d/1dmHlx1WTbH5yZoNa_ln325q5dxLn1QHU/view_`;
   try {
-    let res = await GDriveDl(args[0])
-    if (res.fileSize.slice(-2) == "GB") return m.reply(`لا تقلق.\nأين يمكنني إرسال مقطع فيديو ${res.fileSize}`)
-    if (!someincludes(['kB','KB'], res.fileSize.slice(-2)) && parseInt(res.fileSize) > 100) return m.reply(`حجم الملف: ${res.fileSize}\nغير قادر على الإرسال، الحد الأقصى للملف هو 100 ميجابايت`)
-    let txt = `*[ جاري التحميل ]*\n\n`
-    txt += `*الاسم :* ${res.fileName}\n`
-    txt += `*الحجم :* ${res.fileSize}\n`
-    txt += `*نوع الملف :* ${res.mimetype}`
-    await m.reply(txt)
-    if (!res.downloadUrl) throw eror
-    await conn.sendFile(m.chat, res.downloadUrl, res.fileName + res.mimetype, res.fileName + res.mimetype, m)
+    GDriveDl(args[0]).then(async (res) => {
+      conn.reply(m.chat, tradutor.texto2, m);
+      if (!res) throw res;
+      conn.sendFile(m.chat, res.downloadUrl, res.fileName, '', m, null, {mimetype: res.mimetype, asDocument: true});
+    });
   } catch (e) {
-    console.log(e)
-    throw '*ليس لدى الروبوتات إمكانية الوصول إلى GoogleDrive هذا*'
+    m.reply(tradutor.texto3);
+    console.log(e);
   }
+};
+handler.command = /^(gdrive|درايف)$/i;
+export default handler;
+async function GDriveDl(url) {
+  let id;
+  if (!(url && url.match(/drive\.google/i))) throw 'Invalid URL';
+  id = (url.match(/\/?id=(.+)/i) || url.match(/\/d\/(.*?)\//))[1];
+  if (!id) throw 'ID Not Found';
+  const res = await fetch(`https://drive.google.com/uc?id=${id}&authuser=0&export=download`, {
+    method: 'post',
+    headers: {
+      'accept-encoding': 'gzip, deflate, br',
+      'content-length': 0,
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'origin': 'https://drive.google.com',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+      'x-client-data': 'CKG1yQEIkbbJAQiitskBCMS2yQEIqZ3KAQioo8oBGLeYygE=',
+      'x-drive-first-party': 'DriveWebUi',
+      'x-json-requested': 'true'}});
+  const {fileName, sizeBytes, downloadUrl} = JSON.parse((await res.text()).slice(4));
+  if (!downloadUrl) throw 'Link Download Limit!';
+  const data = await fetch(downloadUrl);
+  if (data.status !== 200) throw data.statusText;
+  return {downloadUrl, fileName, fileSize: formatSize(sizeBytes), mimetype: data.headers.get('content-type')};
 }
-
-handler.help = ['gdrive2']
-handler.tags = ['downloader']
-handler.command = /^(gdrive2|درايف2)$/i
-
-
-export default handler
