7c13b7f85bf7d064eb452bbdcbaf5dabf56764f7
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-18 15:39:14 +0300

    Update اضافه.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/اضافه.js
+++ plugins/اضافه.js
@@ -1,22 +1,45 @@
-let handler = async (m, { conn, args, text, usedPrefix, command }) => {
-let who 
-if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-else who = m.chat
-let name = await conn.getName(m.sender)	
-let user = global.db.data.users[who]
-if (!global.db.data.settings[conn.user.jid].restrict) return conn.reply(m.chat, `${lenguajeGB['smsAvisoAG']()}${lenguajeGB['smsSoloOwner']()}`, fkontak, m) 
-if (!text) throw `${lenguajeGB['smsAvisoMG']()} ${lenguajeGB['smsMalused']()}\n*${usedPrefix + command}* 59355555555`
-if (text.includes('+')) throw  `${lenguajeGB['smsAvisoMG']()} ${lenguajeGB['smsMalused']()}\n*${usedPrefix + command}* 59355555555`
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-await conn.reply(text+'@s.whatsapp.net', `${mid.smsAdd}\n\n${link}`, m, {mentions: [m.sender]})
-m.reply(`*@${who.split`@`[0]}*\n${mid.smsAdd2}`) 
-}
-handler.help = ['add', '+'].map(v => v + ' número')
-handler.tags = ['group']
-handler.command = /^(add|agregarl|invitar|invite|añadir|\+)$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
-export default handler
+
+const {generateWAMessageFromContent, prepareWAMessageMedia, proto} = (await import('@whiskeysockets/baileys')).default;
+import fetch from 'node-fetch';
+const {getBinaryNodeChild, getBinaryNodeChildren} = (await import('@whiskeysockets/baileys')).default;
+const handler = async (m, {conn, text, participants, args}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const tradutor = _translate.plugins.gc_add
+
+  if (!global.db.data.settings[conn.user.jid].restrict) throw tradutor.texto1;
+  if (!args[0]) throw tradutor.texto2;
+  try {
+    const _participants = participants.map((user) => user.id);
+    const users = (await Promise.all(
+        text.split(',')
+            .map((v) => v.replace(/[^0-9]/g, ''))
+            .filter((v) => v.length > 4 && v.length < 20 && !_participants.includes(v + '@s.whatsapp.net'))
+            .map(async (v) => [v, await conn.onWhatsApp(v + '@s.whatsapp.net')]))).filter((v) => v[1][0]?.exists).map((v) => v[0] + '@c.us');
+    const response = await conn.query({tag: 'iq', attrs: {type: 'set', xmlns: 'w:g2', to: m.chat}, content: users.map((jid) => ({tag: 'add', attrs: {}, content: [{tag: 'participant', attrs: {jid}}]}))});
+    const pp = await conn.profilePictureUrl(m.chat).catch((_) => null);
+    const jpegThumbnail = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0);
+    const add = getBinaryNodeChild(response, 'add');
+    const participant = getBinaryNodeChildren(add, 'participant');
+    for (const user of participant.filter((item) => item.attrs.error == 403)) {
+      const jid = user.attrs.jid;
+      const content = getBinaryNodeChild(user, 'add_request');
+      const invite_code = content.attrs.code;
+      const invite_code_exp = content.attrs.expiration;
+      const teks = `${tradutor.texto3[0]} @${jid.split('@')[0]}, ${tradutor.texto3[1]}`;
+      m.reply(teks, null, {mentions: conn.parseMention(teks)});
+      const captionn = tradutor.texto4;
+      const messaa = await prepareWAMessageMedia({image: jpegThumbnail}, {upload: conn.waUploadToServer});
+      const groupInvite = generateWAMessageFromContent(m.chat, proto.Message.fromObject({groupInviteMessage: {groupJid: m.chat, inviteCode: invite_code, inviteExpiration: invite_code_exp, groupName: await conn.getName(m.chat), caption: captionn, jpegThumbnail: jpegThumbnail}}), {userJid: jid});
+      await conn.relayMessage(jid, groupInvite.message, {messageId: groupInvite.key.id});
+    }
+  } catch {
+    throw tradutor.texto5;
+  }
+};
+handler.help = ['add', '+'].map((v) => v + ' número');
+handler.tags = ['group'];
+handler.command = /^(add|اضافه|añadir|\+)$/i;
+handler.admin = handler.group = handler.botAdmin = true;
+export default handler;
