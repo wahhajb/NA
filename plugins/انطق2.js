e4e1a56028895e536aeb96e35738582882f337f2
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-18 15:15:59 +0300

    Update انطق2.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/انطق2.js
+++ plugins/انطق2.js
@@ -1,39 +1,102 @@
-import gtts from  'node-gtts'
-import { readFileSync, unlinkSync } from  'fs' 
-import { join } from  'path'
-const defaultLang =  'ar'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let lang = args[0]
-let text = args.slice(1).join(' ')
-if ((args[0] ||'').length !== 2) {
-lang = defaultLang
-text = args.join('')
-}
-if (!text && m.quoted?.text) text = m.quoted.text
-let res
-try { res = await tts(text, lang) }
-catch (e) {
-m.reply(e +'')
-text = args.join('')
-if (!text) throw `تحويل نص لاوديو مثال \n *.انطق2 هنري أحسن بوت في العالم*`
-res = await tts(text, defaultLang)
-} finally {
-if (res) conn.sendFile(m.chat, res,  'tts.opus', null, m, true)
-}}
-handler.help = ['tts <lang> <teks>']
-handler.tags = ['tools']
-handler.command = /^g?انطق2$/i
-export default handler
-function tts(text, lang ='ar') {
-console.log(lang, text)
-return new Promise((resolve, reject) => {
-try {
-let tts = gtts(lang)
-let filePath = join(global.__dirname(import.meta.url),  '../tmp', (1 * new Date) +  '.wav')
-tts.save(filePath, text, () => {
-resolve(readFileSync(filePath))
-unlinkSync(filePath)
-})
-} catch (e) { reject(e) }
-})}
+Skip to content
+Navigation Menu
 
+Code
+Issues
+3
+Pull requests
+3
+Actions
+Projects
+Wiki
+Security
+Insights
+BreadcrumbsTheMystic-Bot-MD/plugins
+/convertidor-tts2.js
+Latest commit
+ 
+History
+116 lines (97 loc) · 3.75 KB
+File metadata and controls
+
+Code
+
+Blame
+/*//////////////////////////////////////////////
+
+        [ ❗ ] CREDITOS - NO MODIFICAR [ ❗ ]
+
+           Codigo hecho por @BrunoSobrino
+       Github: https://github.com/BrunoSobrino
+       
+       Nota: Solo hay disponibles efectos en
+       ingles, por lo que el texto en otros
+       idiomas puede sonar raro.
+       
+//////////////////////////////////////////////*/
+
+import axios from 'axios';
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, usedPrefix, command, text, args }) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const tradutor = _translate.plugins.convertidor_tts2
+
+
+  const [efecto, ...textoArray] = text.split(" ");
+  const texto = textoArray.join("");
+
+  if (!efecto) {
+    let voiceList = await getVoiceList();
+    let responseText = `*${tradutor.texto1}*\n`;
+
+    for (let i = 0, count = 0; count < 100 && i < voiceList.resultado.length; i++) {
+      const entry = voiceList.resultado[i];
+      if (entry.ID.length <= 20) {
+        responseText += `*◉ ${usedPrefix + command} ${entry.ID} ${tradutor.texto2}*\n`;
+        count++;
+      }
+    }
+
+    return conn.sendMessage(m.chat, { text: responseText.trim() }, { quoted: m });
+  }
+
+  let efectoValido = false;
+  let voiceList = await getVoiceList();
+  for (const entry of voiceList.resultado) {
+    if (entry.ID === efecto) {
+      efectoValido = true;
+      break;
+    }
+  }
+
+  if (!efectoValido) return conn.sendMessage(m.chat, { text: `*${tradutor.texto3[0]} ${usedPrefix + command} ${tradutor.texto3[1]}*` }, { quoted: m });
+
+  if (!texto) return conn.sendMessage(m.chat, {text: `*${tradutor.texto4[0]}*\n*◉ ${usedPrefix + command} ${efecto} ${tradutor.texto4[1]}*`}, {quoted: m});
+
+  let masivo = await makeTTSRequest(texto, efecto);
+  conn.sendMessage(m.chat, {audio: {url: masivo.resultado}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+};
+
+handler.command = /^(انطق2)$/i;
+export default handler;
+
+const secretKey = 'fe2ee40099494579af0ecf871b5af266';
+const userId = 'SrgwcKcLzSY63IdsAxd1PzscFjL2';
+
+async function getVoiceList() {
+  const url = 'https://play.ht/api/v2/voices';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      AUTHORIZATION: `Bearer ${secretKey}`,
+      'X-USER-ID': userId
+    }
+  };
+  try {
+    const response = await fetch(url, options);
+    const responseData = await response.json(); 
+    const uniqueData = responseData.reduce((acc, current) =
