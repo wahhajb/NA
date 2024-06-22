59a523423e22f14f8142f0881192d4d5a9128c0c
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:49:43 +0300

    Update كانيكي.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/كانيكي.js
+++ plugins/كانيكي.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-keneki.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*كانيكي 🥱*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `_*كـانـيـكـي 🥱*_`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['anna']
 handler.tags = ['internet']
