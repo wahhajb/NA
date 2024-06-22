7cc52e753fddd1009bfcd80cec92e68819a4a34a
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:45:40 +0300

    Update هيستيا.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/هيستيا.js
+++ plugins/هيستيا.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-hestia.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*هيستيا 🌚❤️*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `*_هـيـسـتـيا 🌚❤️_*`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['anna']
 handler.tags = ['internet']
