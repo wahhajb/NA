51f101acfd959b7692ecdbb7c536cc2c571357eb
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:37:07 +0300

    Update ايوزاوا.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/ايوزاوا.js
+++ plugins/ايوزاوا.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-ayuzawa.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*ايـوزاوا 😽*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `*_ايـوزاوا 😽_*`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['anna']
 handler.tags = ['internet']
