0ceabdf4e99d737ac67cdb733d9f92b289692b0c
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:33:01 +0300

    Update اكياما.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/اكياما.js
+++ plugins/اكياما.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-akiyama.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*اكـيـامـا 🐤*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `_*اكـيـامـا 🐤*_`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['اكياما']
 handler.tags = ['internet']
