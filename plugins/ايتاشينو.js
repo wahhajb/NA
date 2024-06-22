47b89cd997dedfb91051bbfff140fdfc3067e015
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:47:51 +0300

    Update ايتاشينو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/ايتاشينو.js
+++ plugins/ايتاشينو.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-itachi.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*إيتاشي 🤤*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `_*إيـتـاشـي 🤤*_`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['anna']
 handler.tags = ['internet']
