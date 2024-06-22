95f7a8c485c2d965e42075a87242edf635041973
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:46:31 +0300

    Update هيناتا.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/هيناتا.js
+++ plugins/هيناتا.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-hinata.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*هيناتا 😽*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `_*هـيـنـاتـا 😽*_`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['anna']
 handler.tags = ['internet']
