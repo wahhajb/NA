ed273a922a4cd1932cb04a0e64a3dffc0bfe70a1
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 08:45:44 +0300

    Create نيزيكو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

---
+++ plugins/نيزيكو.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-nezuko.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*الحلوه نيكزوكو 🥺❤️*`, m)}
+//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+handler.help = ['نيزيكو']
+handler.tags = ['internet']
+handler.command = /^(نيزيكو|نيزوكو)$/i
+export default handler
