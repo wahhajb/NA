d8b1ca1acc720cf90ba2e436dfa155e1f1bac83d
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 08:42:36 +0300

    Update الدعم.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/الدعم.js
+++ plugins/الدعم.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 let handler = async (m, {command, conn, usedPrefix}) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/socona12/TheMystic-Bot-MD/master/src/JSON/anime-Venome.json`)).data  
+let res = (await axios.get(`https://raw.githubusercontent.com/socona12/TheMystic-Bot-MD/master/src/JSON/anime-Venom.json`)).data  
 let haha = await res[Math.floor(res.length * Math.random())]    
 conn.sendFile(m.chat, haha, 'error.jpg', `
 
