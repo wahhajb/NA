6cacb72cee64752a8b64c520376388f373a58d9f
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 12:51:43 +0300

    Create كورومي.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

---
+++ plugins/كورومي.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-kurumi.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `*_كـورومـي 😵_*`, m)}
+//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
+handler.help = ['anna']
+handler.tags = ['internet']
+handler.command = /^(kurumi|كورومي)$/i
+export default handler
