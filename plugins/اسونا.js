4cdcacda5d383d8511805e77cf105d0d296f54ae
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:29:55 +0300

    Update اسونا.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/اسونا.js
+++ plugins/اسونا.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-asuna.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*اسـونـا 🌝*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `_*اسـونـا 🌝*_`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['anna']
 handler.tags = ['internet']
