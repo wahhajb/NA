da3fc77838c84578b1b6d7f021d499e9e956a36b
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:51:06 +0300

    Create اينوري.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

---
+++ plugins/اينوري.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-inori.json`)).data  
+let url = await res[Math.floor(res.length * Math.random())]
+conn.sendFile(m.chat, url, 'error.jpg', `_*ايـنـوري 🥱*_`, m)}
+//conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
+handler.help = ['anna']
+handler.tags = ['internet']
+handler.command = /^(inori|اينوري)$/i
+export default handler
