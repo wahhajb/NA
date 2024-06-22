d6e2314d68b300f9ed28b8f7d9edc2b478d48b42
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:43:57 +0300

    Update ايميلي.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/ايميلي.js
+++ plugins/ايميلي.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-emilia.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*ايميلي الحلوه 🥺*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `_*ايميلي الحلوه 🥺*_`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['anna']
 handler.tags = ['internet']
