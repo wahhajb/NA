b7eb7b544a17d7d9370d56f84cb98f04d1baddae
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-22 11:40:07 +0300

    Update تشيهو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/تشيهو.js
+++ plugins/تشيهو.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-chiho.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
-conn.sendFile(m.chat, url, 'error.jpg', `*تشيهو عشوائيه 🌚*`, m)}
+conn.sendFile(m.chat, url, 'error.jpg', `_*تشيهو عشوائيه 🌚_*`, m)}
 //conn.sendButton(m.chat, "*Messi*", author, url, [['⚽ Seguente', `${usedPrefix + command}`]], m)}
 handler.help = ['anna']
 handler.tags = ['internet']
