65fc1215aaa23ce97d0fc9ae550cb1b6e21ef244
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-24 00:09:30 +0300

    Create تجربت.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

---
+++ plugins/تجربت.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+    let message = `*البــنــك*`;
+    await conn.sendMessage(m.chat, {
+   react: {
+ text: "⛪",
+ key: m.key,
+   }
+  })
+
+    conn.sendFile(m.chat, 'https://66394f4ae0ff6.site123.me/', 'video.mp4' , message, m);
+};
+
+handler.customPrefix = /^(bot|بوت)$/i;
+handler.command = new RegExp;
+
+export default handler;
