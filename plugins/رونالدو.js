2b3d5e869360fdf74a638232e39f044bb34e3610
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-23 09:57:02 +0300

    Update رونالدو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/رونالدو.js
+++ plugins/رونالدو.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
 let cristiano = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/CristianoRonaldo.json`)).data  
 let ronaldo = await cristiano[Math.floor(cristiano.length * Math.random())]
-conn.sendFile(m.chat, ronaldo, 'error.jpg', `*رونالدو الحب*`, m)}
+conn.sendFile(m.chat, ronaldo, 'error.jpg', `_*رونــالدو الحب*_`, m)}
 //conn.sendButton(m.chat, "*Siiiuuuuuu*", author, ronaldo, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `${usedPrefix + command}`]], m)}
 handler.help = ['cristianoronaldo', 'cr7']
 handler.tags = ['internet']
