a92a436ecb26253be05428d6f54665a6ca7e3985
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-24 00:02:13 +0300

    Update اجابه.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/اجابه.js
+++ plugins/اجابه.js
@@ -6,7 +6,7 @@ export async function before(m) {
         return !0
     this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
     if (!(id in this.tebakbendera))
-        return this.reply(m.chat, '*لقد انتهي هذا السؤال اكتب احزر او رياضة لتظهر أسأله جديده*', m)
+        return this.reply(m.chat, '*لقد انتهي هذا السؤال اكتب علم لتظهر أسأله جديده*', m)
     if (m.quoted.id == this.tebakbendera[id][0].id) {
         let isSurrender = /^(انسحب|surr?ender)$/i.test(m.text)
         if (isSurrender) {
@@ -15,13 +15,13 @@ export async function before(m) {
             return this.reply(m.chat, '*طلع فاشل و استسلم :( !*', m)
         }
         let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
-        
-        if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
+
+        if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
             this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐↞┇الـجـائـزة💰↞${this.tebakbendera[id][2]} نقطه*`, m)
             clearTimeout(this.tebakbendera[id][3])
             delete this.tebakbendera[id]
-        } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold)
+        } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
             m.reply(`*لقد كنت علي وشك النجاح*!`)
         else
             this.reply(m.chat, `❐┃اجـابـة خـاطـئـة┃❌ ❯`, m)
