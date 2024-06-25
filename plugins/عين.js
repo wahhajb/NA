c98d2c1a2248eb9058c933570f4aa683c2268488
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-25 15:03:03 +0300

    Update عين.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/عين.js
+++ plugins/عين.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://gist.githubusercontent.com/YosefZoro1/2e1ba4a3f14368c5620ef099ac21bcdb/raw/294a1b3e4114f4937382a285534bdcacc06917de/game6.json')).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/DK3MK/worker-bot/main/eye.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*${command.toUpperCase()}*
   ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)}* ┇
