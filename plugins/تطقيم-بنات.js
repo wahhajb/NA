0fc47504bc06d559b74b28c9b2badb279b1d03ba
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-25 15:18:46 +0300

    Update تطقيم-بنات.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/تطقيم-بنات.js
+++ plugins/تطقيم-بنات.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('https://raw.githubusercontent.com/Afghhjjkoo/GURU-BOT/main/lib/miku54.json)).json()
+  let data = await (await fetch('https://raw.githubusercontent.com/Afghhjjkoo/GURU-BOT/main/lib/miku54.json')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
 
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '*بنت*👧🏻\n𝐵𝑌:𝑍𝑂𝑅𝑂⚡𝐵𝑂𝑇', m)
+  await conn.sendFile(m.chat, cowi, '', '*بنت*👧🏻\nBy:SUKUNA BOT', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', '*بنت*👧\n𝐵𝑌:𝑍𝑂𝑅𝑂⚡𝐵𝑂𝑇', m)
+  await conn.sendFile(m.chat, ciwi, '', '*بنت*👧\nBY:SUKUNA BOT', m)
 }
 handler.help = ['Miku bot']
 handler.tags = ['Miku bot']
