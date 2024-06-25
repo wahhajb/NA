7bf53f4d4de86a988534778326ed597cd821ac6a
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-25 16:28:03 +0300

    Update تغير_الترحيب.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/تغير_الترحيب.js
+++ plugins/تغير_الترحيب.js
@@ -1,11 +1,15 @@
+//import db from '../lib/database.js'
+
 let handler = async (m, { conn, text, isROwner, isOwner }) => {
-if (text) {
-global.db.data.chats[m.chat].sWelcome = text
-m.reply('*تم تغيير الترحيب بنجاح. [❗]*')
-} else throw `*[❗]يرجى كتابة رساله لتغير الترحيب, استخدم:*\n*- @user (منشن الشخص الذي دخل)*\n*- @group (اسم القروب)*\n*- @desc (وصف القروب)*`
+  if (text) {
+    global.db.data.chats[m.chat].sWelcome = text
+    m.reply('*[✅] تم تغير الترحيب يحب*')
+  } else throw `*[❗] ادخل رساله الترحيب الذي تريدها, مثال:*\n*❈↲ .تغير-الترحيب اهلا بك في مجموعتنا*\n*- @user (منشن)*\n*- @group (اسم الجروب)*\n*- @desc (الوصف)*`
 }
-handler.help = ['<text> تغيرالترحيب']
+handler.help = ['setwelcome <text>']
 handler.tags = ['group']
 handler.command = ['تغيرالترحيب'] 
 handler.admin = true
+handler.owner = false
+
 export default handler
