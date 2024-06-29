288bb8ec044e11586bcb86bc11b6e899072969fb
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-06-30 00:22:33 +0300

    Update اضافه.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/اضافه.js
+++ plugins/اضافه.js
@@ -1,15 +1,9 @@
-
 const {generateWAMessageFromContent, prepareWAMessageMedia, proto} = (await import('@whiskeysockets/baileys')).default;
 import fetch from 'node-fetch';
 const {getBinaryNodeChild, getBinaryNodeChildren} = (await import('@whiskeysockets/baileys')).default;
 const handler = async (m, {conn, text, participants, args}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
-  const tradutor = _translate.plugins.gc_add
-
-  if (!global.db.data.settings[conn.user.jid].restrict) throw tradutor.texto1;
-  if (!args[0]) throw tradutor.texto2;
+  if (!global.db.data.settings[conn.user.jid].restrict) throw '*[🚯] المطور قافل الاضافه*';
+  if (!args[0]) throw '*[🚨] ضيف الرقم الذي ترغب في اضافته*';
   try {
     const _participants = participants.map((user) => user.id);
     const users = (await Promise.all(
@@ -27,19 +21,19 @@ const handler = async (m, {conn, text, participants, args}) => {
       const content = getBinaryNodeChild(user, 'add_request');
       const invite_code = content.attrs.code;
       const invite_code_exp = content.attrs.expiration;
-      const teks = `${tradutor.texto3[0]} @${jid.split('@')[0]}, ${tradutor.texto3[1]}`;
+      const teks = `*[❗معلومه❗]لايوجد اعلان حاليا @${jid.split('@')[0]}, أضف الرقم الذي أدخلته، قد يحدث هذا لأن الرقم غير صحيح، أو أن الشخص غادر المجموعة مؤخرًا، أو أن الشخص غادر المجموعة مؤقتًا، أو أن الشخص قد قام بتغيير خصوصية المجموعة. أنصحك بالتحقق من الدعوة يدويًا*`;
       m.reply(teks, null, {mentions: conn.parseMention(teks)});
-      const captionn = tradutor.texto4;
+      const captionn = `🚩 مرحبا انا ميجو لقد تم دعوتك اللي الجروب لذالك قمت بارسال الرابط اليك !!`;
       const messaa = await prepareWAMessageMedia({image: jpegThumbnail}, {upload: conn.waUploadToServer});
       const groupInvite = generateWAMessageFromContent(m.chat, proto.Message.fromObject({groupInviteMessage: {groupJid: m.chat, inviteCode: invite_code, inviteExpiration: invite_code_exp, groupName: await conn.getName(m.chat), caption: captionn, jpegThumbnail: jpegThumbnail}}), {userJid: jid});
       await conn.relayMessage(jid, groupInvite.message, {messageId: groupInvite.key.id});
     }
   } catch {
-    throw tradutor.texto5;
+    throw '*[❗معلومه❗] أضف الرقم الذي أدخلته، قد يحدث هذا لأن الرقم غير صحيح، أو أن الشخص غادر المجموعة مؤخرًا، أو أن الشخص غادر المجموعة مؤقتًا، أو أن الشخص قد قام بتغيير خصوصية المجموعة. أنصحك بالتحقق من الدعوة يدويًا*';
   }
 };
 handler.help = ['add', '+'].map((v) => v + ' número');
 handler.tags = ['group'];
-handler.command = /^(add|اضافه|añadir|\+)$/i;
+handler.command = /^(اضافه|añadir|\+)$/i;
 handler.admin = handler.group = handler.botAdmin = true;
 export default handler;
