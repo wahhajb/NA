e857bd76e53c5ffa69902868c50fccc6a6a33979
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-02 01:30:02 +0300

    Update التحذيرات.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/التحذيرات.js
+++ plugins/التحذيرات.js
@@ -1,19 +1,19 @@
 
 let handler = async (m, { conn, args, groupMetadata}) => {
        let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-       if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+       if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات`
        let warn = global.db.data.users[who].warn
        let name = conn.getName(who)
       m.reply(`
- *الانذارات*
+ *التحذيرات*
 
-⚡ *الاسم :* ${name} 
-⚠️ *الانذارات :* ${warn}`)
+▢ *الاسم :* ${name} 
+▢ *التحذيرات :* ${warn}`)
 }
 
 handler.help = ['warns']
 handler.tags = ['group']
-handler.command = ['التحذيرات' ,'الانذارات'] 
+handler.command = ['الانذارات', 'التحذيرات'] 
 handler.group = true
 
 export default handler
