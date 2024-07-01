221412d99a4c621a36e0d3c1b7fb028d6bfe016a
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-01 09:02:43 +0300

    Update وايفو.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/وايفو.js
+++ plugins/وايفو.js
@@ -11,7 +11,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   if (!res.ok) throw await res.text();
   const json = await res.json();
   if (!json.url) throw 'Error!';
-  conn.sendFile(m.chat, json.url, 'error.jpg', tradutor.texto1, m);
+  conn.sendFile(m.chat, _*وايــفــو*_, 'error.jpg', tradutor.texto1, m);
 // conn.sendButton(m.chat, `🥵❤️`, author, json.url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)
 };
 handler.help = ['waifu'];
