659e327d6e65c7500bcc501c5e1ebd77560cccce
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-02 19:09:20 +0300

    Update JOHAN-antiprivate.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/JOHAN-antiprivate.js
+++ plugins/JOHAN-antiprivate.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[😊] اهلا يا @${m.sender.split`@`[0]}, ممنوع التحدث مع البوت في الدردشه الخاص و الان سيتم حظرك\nراسل المطور لكي يدخل لك البوت في جروبك\nرقمه: +994404282709*`, false, {mentions: [m.sender]});
+    await m.reply(`*😐 اهلا يا @${m.sender.split`@`[0]}, ممنوع التحدث مع البوت في الدردشه الخاص و الان سيتم حظرك*\n*راسل المطور لكي يدخل لك البوت في جروبك*\n*رقمه: +994404282709*`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
