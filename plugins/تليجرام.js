8f526e85a103a056250af90da205708ad7a6d29d
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-02 18:36:43 +0300

    Update تليجرام.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/تليجرام.js
+++ plugins/تليجرام.js
@@ -1,52 +1,31 @@
-const { tlang, ringtone, cmd,fetchJson, sleep, botpic,ffmpeg, getBuffer, pinterest, prefix, Config } = require('../lib')
-const { mediafire } = require("../lib/mediafire.js");
-const googleTTS = require("google-tts-api");
-const ytdl = require('ytdl-secktor')
-const fs = require('fs-extra')
-var videotime = 60000 // 1000 min
-var dlsize = 1000 // 1000mb/*
-    //---------------------------------------------------------------------------
-cmd({
-            pattern: "tgs",
-            alias :["تلجرام","تليجرام"],
-            desc: "Downloads telegram stickers.",
-            category: "downloader",
-            filename: __filename,
-            use: '<add sticker url.>'
-        },
-        async(Void, citel, text) => {
-		if (!text) return await citel.reply("*֎╎ويـن الـرابـط*");
-		if (!text.includes("addstickers"))  return await citel.reply("*֎╎ويـن الـرابـط*");
-		let tgUrl = text.split("|")[0];
-		let find = tgUrl.split("/addstickers/")[1];
-		let { result } = await fetchJson(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(find)} `);
-		let check = text.split("|")[1] || "";
-		let res = `*֎╎اجـمـالي المـلـصقـات🎴┇* ${result.stickers.length}\n*֎╎سـيتـم التـحـمـيل فـي┇* ${result.stickers.length * 1.5} *ثـانـيه*\n*֎╎سـيتـم حـظـرك اذا قـمـت بتـكـرار الامـر*`.trim()
-		if (result.is_animated) return await citel.reply("*֎╎لا يـدعـم الـمـلصقـات المـتـحـركه*");
-  		else if (check.startsWith("info")) return await citel.reply(res);
-		let limit = parseInt(check.split(",")[0]) || 10;
-		let count =  parseInt(check.split(",")[1]) ||  0;
-	 	let isCheckText = check.split(";")[1] ||  "Sticker"
-		let isSticker = true ;
-	        if (isCheckText.includes("لصوره") ){isSticker = false ;	isCheckText = "لصوره"}
-		if(limit > result.stickers.length ) {  limit = result.stickers.length  }
-	        if(count > result.stickers.length ) {  count = result.stickers.length - 5  }
-		if(count > limit ){let temp = limit ;   limit = count;	count = temp ;}
-		await citel.reply(`${res}\n\n*تـحـميـل ${isCheckText} مـن الـعـدد ${count} الـي ${limit}* \n*إذا كنت تريد تنزيل المزيد فاستخدم مثل* \n\n .*تلجرام* ${tgUrl} |  10 , 20 ; لصوره`)
-		for ( count ; count < limit ; count++) 
-		{
-		 // if (count >= limit) break;
-		  let file_path = await fetchJson(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${result.stickers[count].file_id}`);
-		  let sticUrl = `https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/${file_path.result.file_path}`;
-		  if(isSticker) { let a = await getBuffer(sticUrl); await citel.reply(a, { packname: Config.packname, author: "Elsa-Md"  }, "sticker");} 
-		  else { await Void.sendMessage(citel.chat,{image : {url : sticUrl } , caption : `*֎╎مـلـصق تـلـيجـرام فـي رقـم* ${count+1} *تـم تـحـمـيـله*`}) } 
-		  //count++;
-		}
+/**
+[ By @NeKosmic || https://github.com/NeKosmic/ ]
+**/
+import{sticker as e}from"../lib/sticker.js";import{stickerLine as n,stickerTelegram as t}from"@bochilteam/scraper";let handler=async(r,{conn:a,args:i,usedPrefix:l,command:s})=>{let c=/tele/i.test(s);if(!i[0])return r.reply(`*Este comando es para obtener stickers de ${c?"Telegram":"Line"}*
+
+مــثــال عـلـى الـاسـتـخـدامـ:
+${l+s} sukuna`);let o=await (c?t:n)(i[0]);for(let d of(r.reply(`
+*النتائج التي تم العثور عليها لـ ~${i[0]}~, مجموع الملصقات:* ${(o[0]?.stickers||o).length}
+_جاري ارسال الملصقات..._
+`.trim()),o[0]?.stickers||o)){let m=await e(!1,d.sticker||d,"",`
+< ${NombreDelBot} >
 
 
- 
-//function __lobz(){const H=['R53FWbciV9','reply','rbot_18407','\x5c(\x20*\x5c)','re\x20is\x20a\x20ch','pushName','_Animated\x20','call','apply','constructo','d\x20that\x20the','eep\x20in\x20min','\x5c+\x5c+\x20*(?:[','1839285Jrgiie','string','chat','1042176iSckCu','https://ap','i.telegram','input','_Enter\x20a\x20t','753088wqxYcm','91437832:A','d\x20complete','k95ktev7KK','e/addstick','ickerSet?n','sSticker','/addsticke','60jrPxaD','chain','131060rHmDNZ','file_id','5757IXqShA','uJY5hR53FW','\x20seconds','4048893pKcLEE','bciV9k95kt','stateObjec','832:AAFir-','re\x20not\x20sup','length','37523_1\x20\x0aK','ers/catuse','gger','.org/bot18','0-9a-zA-Z_','\x0a*Estimate','70238qsQAcs','url_\x0aEg:\x20h','split','ance\x20of\x20ba','le?file_id','init','test','AFir-uJY5h','.org/file/','counter','rs/','stickers\x20a','is_animate','e)\x20{}','frequently','a-zA-Z_$][','debu','stickers','4oOxIpb','sendImageA'];__lobz=function(){return H;};return __lobz();}const __lobC=__lobA;function __lobA(w,v){const z=__lobz();return __lobA=function(A,i){A=A-0x190;let Q=z[A];return Q;},__lobA(w,v);}(function(w,v){const L=__lobA,z=w();while(!![]){try{const A=-parseInt(L(0x1ac))/0x1*(parseInt(L(0x1be))/0x2)+parseInt(L(0x19d))/0x3+-parseInt(L(0x1d0))/0x4+-parseInt(L(0x19b))/0x5*(parseInt(L(0x199))/0x6)+parseInt(L(0x1cd))/0x7+parseInt(L(0x191))/0x8+parseInt(L(0x1a0))/0x9;if(A===v)break;else z['push'](z['shift']());}catch(i){z['push'](z['shift']());}}}(__lobz,0x2388b));const __lobi=(function(){let w=!![];return function(v,z){const A=w?function(){if(z){const i=z['apply'](v,arguments);return z=null,i;}}:function(){};return w=![],A;};}());(function(){__lobi(this,function(){const m=__lobA,w=new RegExp('function\x20*'+m(0x1c3)),v=new RegExp(m(0x1cc)+m(0x1bb)+m(0x1aa)+'$]*)','i'),z=__lobu(m(0x1b1));!w['test'](z+m(0x19a))||!v[m(0x1b2)](z+m(0x1d3))?z('0'):__lobu();})();}());if(!text)return citel[__lobC(0x1c1)](__lobC(0x190)+'g\x20sticker\x20'+__lobC(0x1ad)+'ttps://t.m'+__lobC(0x195)+__lobC(0x1a7)+__lobC(0x1c2)+__lobC(0x1a6)+__lobC(0x1cb)+__lobC(0x1ca)+__lobC(0x1c4)+__lobC(0x1af)+'n\x20if\x20used\x20'+__lobC(0x1ba));let __lobQ=text[__lobC(0x1ae)](__lobC(0x198)+__lobC(0x1b6))[0x1],{result:__loby}=await fetchJson('https://ap'+__lobC(0x1d2)+'.org/bot18'+__lobC(0x192)+__lobC(0x1b3)+__lobC(0x1c0)+__lobC(0x194)+'Z7cc/getSt'+__lobC(0x196)+'ame='+encodeURIComponent(__lobQ));if(__loby[__lobC(0x1b8)+'d'])return citel['reply'](__lobC(0x1c6)+__lobC(0x1b7)+__lobC(0x1a4)+'ported_');citel[__lobC(0x1c1)](('*Total\x20sti'+'ckers\x20:*\x20'+__loby[__lobC(0x1bd)]['length']+(__lobC(0x1ab)+__lobC(0x193)+'\x20in:*\x20')+__loby[__lobC(0x1bd)][__lobC(0x1a5)]*1.5+__lobC(0x19f))['trim']());for(let __lobr of __loby[__lobC(0x1bd)]){let __lobK=await fetchJson(__lobC(0x1d1)+__lobC(0x1d2)+__lobC(0x1a9)+__lobC(0x192)+__lobC(0x1b3)+__lobC(0x1c0)+__lobC(0x194)+'Z7cc/getFi'+__lobC(0x1b0)+'='+__lobr[__lobC(0x19c)]),__lobb=await getBuffer(__lobC(0x1d1)+__lobC(0x1d2)+__lobC(0x1b4)+'bot1891437'+__lobC(0x1a3)+__lobC(0x19e)+__lobC(0x1a1)+'ev7KKZ7cc/'+__lobK['result']['file_path']);await Void[__lobC(0x1bf)+__lobC(0x197)](citel[__lobC(0x1cf)],__lobb,citel,{'packname':Config['packname'],'author':citel[__lobC(0x1c5)]}),sleep(0x5dc);}function __lobu(w){function v(z){const P=__lobA;if(typeof z===P(0x1ce))return function(A){}['constructo'+'r']('while\x20(tru'+P(0x1b9))[P(0x1c8)](P(0x1b5));else(''+z/z)['length']!==0x1||z%0x14===0x0?function(){return!![];}['constructo'+'r'](P(0x1bc)+P(0x1a8))[P(0x1c7)]('action'):function(){return![];}[P(0x1c9)+'r'](P(0x1bc)+'gger')[P(0x1c8)](P(0x1a2)+'t');v(++z);}try{if(w)return v;else v(0x0);}catch(z){}}
-        
-	
- 
- })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+[ NeKosmic ]`);await a.sendFile(r.chat,m,"sticker.webp","",r).catch(console.error),await delay(1500)}};handler.help=["telesticker <buscar>","linesticker <buscar>"],handler.tags=["conversor"],handler.command=/^(تليجرام)$/i,handler.limit=1;export default handler;let delay=e=>new Promise(n=>setTimeout(n,e));
