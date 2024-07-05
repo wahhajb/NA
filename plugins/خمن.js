c0b63ff8eee2c20509109188658e54eac6fec403
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-06 00:21:46 +0300

    Update خمن.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/خمن.js
+++ plugins/خمن.js
@@ -1,40 +1,39 @@
-import fs from 'fs'
+import fs from 'fs';
 
-let timeout = 60000
-let poin = 50000
+let timeout = 60000;
+let poin = 50000;
 
 let handler = async (m, { conn, usedPrefix }) => {
-    conn.tekateki = conn.tekateki ? conn.tekateki : {}
-    let id = m.chat
+    conn.tekateki = conn.tekateki ? conn.tekateki : {};
+    let id = m.chat;
     if (id in conn.tekateki) {
-        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tekateki[id][0])
-        throw false
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tekateki[id][0]);
+        throw false;
     }
-    let tekateki = JSON.parse(fs.readFileSync(`./src/game/خمن.json`))
-    let json = tekateki[Math.floor(Math.random() * tekateki.length)]
-    let _clue = json.response
-    let clue = _clue.replace(/[A-Za-z]/g, '_')
-    let caption = `*خمن اسم الشخصية*
+    let tekateki = JSON.parse(fs.readFileSync(`./src/game/خمن.json`));
+    let json = tekateki[Math.floor(Math.random() * tekateki.length)];
+    let _clue = json.response;
+    let clue = _clue.replace(/[A-Za-z]/g, ''); // Fixed this line
+    let caption = `
+ⷮ ${json.question}
 
-ⷮ *${json.question}*
-
-*❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇*
-*❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇*
+❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇
+❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
 
 *SUKUNA BOT*
-`.trim()
+`.trim();
     conn.tekateki[id] = [
        await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(async () => {
-            if (conn.tekateki[id]) await conn.reply(m.chat, `*❮ ⌛┇انتهي الوقت┇⌛❯*\n *❐↞┇الاجـابـة✅↞ ${json.response}┇*`, conn.tekateki[id][0])
-            delete conn.tekateki[id]
+            if (conn.tekateki[id]) await conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n ❐↞┇الاجـابـة✅↞ ${json.response}┇`, conn.tekateki[id][0]);
+            delete conn.tekateki[id];
         }, timeout)
-    ]
-}
+    ];
+};
 
-handler.help = ['acertijo']
-handler.tags = ['game']
-handler.command = /^(خمن|تخمين)$/i
+handler.help = ['miku'];
+handler.tags = ['game'];
+handler.command = /^(خمن)$/i;
 
-export default handler
+export default handler;
