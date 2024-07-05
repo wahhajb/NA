c890b3f32433eff239d6558e3df8f7bcbac4c099
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-05 14:58:42 +0300

    Update and rename JOHAN-cah.js to خمن.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/خمن.js
+++ plugins/خمن.js
@@ -14,12 +14,13 @@ let handler = async (m, { conn, usedPrefix }) => {
     let json = tekateki[Math.floor(Math.random() * tekateki.length)]
     let _clue = json.response
     let clue = _clue.replace(/[A-Za-z]/g, '_')
-    let caption = `خمن اسم الشخصيه
-    
-    *${json.question}*
+    let caption = `*خمن اسم الشخصية*
+
+ⷮ *${json.question}*
 
 *❐↞┇الـوقـت⏳↞ ${(timeout / 1000).toFixed(2)}┇*
 *❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇*
+
 *SUKUNA BOT*
 `.trim()
     conn.tekateki[id] = [
