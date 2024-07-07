0f5d0942a985fd094637d34951c0d310e3103bee
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-07 21:08:48 +0300

    Update زوجني.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/زوجني.js
+++ plugins/زوجني.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/زوجني.json`)).data  
+let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/zoj.json`)).data  
 let url = await res[Math.floor(res.length * Math.random())]
 conn.sendFile(m.chat, url, 'error.jpg', `*اخليها:-
 تم يا ${taguser} شوف صوره زوجتك/زوجك 🌚♥️🎀*`, m)}
