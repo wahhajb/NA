676f2aa453943d09adfa394f06746f95753fb2e1
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-07 23:37:13 +0300

    Update JOHAN-sticker.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/JOHAN-sticker.js
+++ plugins/JOHAN-sticker.js
@@ -7,6 +7,7 @@ let handler = async (m, { conn }) => {
     let sticker = await conn.sticker(res.data, false, { packname: 'Sticker Pack', author: '@example' })
     await conn.sendMessage(m.chat, sticker, MessageType.sticker, { quoted: m })
 }
-
+handler.help = ['name']
+handler.tag = ['tools']
 handler.command = /^(يوهان)$/i
 module.exports = handler
