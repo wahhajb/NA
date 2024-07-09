a3b868d6c75b803aca81ed9c51a5b5c1a386dc61
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-09 21:01:37 +0300

    Update رمزى.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/رمزى.js
+++ plugins/رمزى.js
@@ -1,10 +1,20 @@
-import { createHash } from 'crypto'
-let handler = async function (m, { conn, text, usedPrefix }) {
-let sn = createHash('md5').update(m.sender).digest('hex')
-await m.reply(`⬇️ *هذا هو الرقم التسلسلي الخاص بك* ⬇️`.trim())
-await m.reply(`${sn}`.trim())
-}
-handler.help = ['myns']
-handler.tags = ['xp']
-handler.command = /^(myns|الرقم-التسسلسي|رمزي|رمزى)$/i
-export default handler
+import {createHash} from 'crypto';
+
+
+const handler = async function(m, {conn, text, usedPrefix}) {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const tradutor = _translate.plugins.rpg_myns
+
+  const sn = createHash('md5').update(m.sender).digest('hex');
+  m.reply(`┏┅ ━━━━━━━━━━━━ ┅ ━
+┃${tradutor.texto1} 
+┃ ${sn}
+┗┅ ━━━━━━━━━━━━ ┅ ━`.trim());
+};
+handler.help = ['myns'];
+handler.tags = ['xp'];
+handler.command = /^(myns|رمزي|رمزى|رقم التسلسل)$/i;
+handler.register = true;
+export default handler;
