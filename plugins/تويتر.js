76d58411fd488ee61b8b89495cafdc3695517b9b
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-11 04:48:21 +0300

    Update تويتر.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/تويتر.js
+++ plugins/تويتر.js
@@ -1,29 +1,128 @@
-import fetch from 'node-fetch'
-import axios from 'axios'
-import cheerio from 'cheerio'
-import vm from 'node:vm'
-import qs from 'qs'
-const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"
-}
-const twitterUrlRegex = /^https?:\/\/(www\.)?twitter\.com\/(\w+)\/status\/(\d+)$/i
-if (!text) return conn.reply(m.chat,`هذا الامر خاص بالتحميل من منصة تويتر مثال نكتب هكذا \n*.twitter*  https://twitter.com/Animalesybichos/status/1564616107159330816\n`, fkontak)
-try{ 
-const apiUrl = `https://api.lolhuman.xyz/api/twitter?apikey=GataDios&url=${encodeURIComponent(text)}`
-const response = await fetch(apiUrl)
-const jsonData = await response.json()
-const tweetData = jsonData.result
-const tweetTitle = tweetData.title
-const tweetVideoUrl = tweetData.media[0].url
-const shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${text}`)).text()
-const tweetTitleWithoutUrl = tweetTitle.replace(/https?:\/\/t\.co\/\w+/i, '').trim()
-const txt1 = `😁 ${tweetTitleWithoutUrl}\n\n🔗 *رابط مختصر للفيديو:*\n• _${shortUrl1}_\n\ninstagram.com)alkaser7008`.trim()
-await conn.sendFile(m.chat, tweetVideoUrl, 'error.mp4', txt1, fkontak)
-} catch (e) {
-console.log(e)
-}}
+import axios from 'axios';
 
-handler.help = ["twitter"]
-handler.tags = ["downloader"]
-handler.command = /^(تويتر)$/i
-export default handler
+let enviando = false;
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar2.json`))
+  const tradutor = _translate.plugins.downloader_x_twitter
+
+
+if (!text) throw `${tradutor.texto1} ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw`;
+if (enviando) return;
+    enviando = true;
+try {
+   await conn.sendMessage(m.chat, {text: global.wait}, {quoted: m}); 
+   const res = await TwitterDL(text);
+ if (res?.result.type == 'video') {
+     const caption = res?.result.caption ? res.result.caption : tradutor.texto2;
+     for (let i = 0; i < res.result.media.length; i++) {
+     await conn.sendMessage(m.chat, {video: {url: res.result.media[i].result[0].url}, caption: caption}, {quoted: m});
+     };
+     enviando = false;
+     return;
+ } else if (res?.result.type == 'photo') {
+     const caption = res?.result.caption ? res.result.caption : tradutor.texto2;
+     for (let i = 0; i < res.result.media.length; i++) {
+     await conn.sendMessage(m.chat, {image: {url: res.result.media[i].url}, caption: caption}, {quoted: m});
+     };
+     enviando = false;
+     return;
+  }
+} catch {
+    enviando = false;
+    throw tradutor.texto3;
+    return;
+  }
+};    
+handler.command = /^(تويتر)$/i;
+export default handler;
+
+const _twitterapi = (id) => `https://info.tweeload.site/status/${id}.json`;
+const getAuthorization = async () => {
+    const { data } = await axios.default.get("https://pastebin.com/raw/SnCfd4ru");
+    return data;
+};
+const TwitterDL = async (url) => {
+  return new Promise(async (resolve, reject) => {
+    const id = url.match(/\/([\d]+)/);
+    if (!id)
+      return resolve({
+        status: "error",
+        message:
+          tradutor.texto4,
+      });
+      const response = await axios.default(_twitterapi(id[1]), {
+        method: "GET",
+        headers: {
+          Authorization: await getAuthorization(),
+          "user-agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
+        },
+      });
+
+      if (response.data.code !== 200) {
+        return resolve({
+          status: "error",
+          message: tradutor.texto5,
+        });
+      }
+
+      const author = {
+        id: response.data.tweet.author.id,
+        name: response.data.tweet.author.name,
+        username: response.data.tweet.author.screen_name,
+        avatar_url: response.data.tweet.author.avatar_url,
+        banner_url: response.data.tweet.author.banner_url,
+      };
+
+      let media = [];
+      let type;
+
+      if (response.data.tweet?.media?.videos) {
+        type = "video";
+        response.data.tweet.media.videos.forEach((v) => {
+          const resultVideo = [];
+          v.video_urls.forEach((z) => {
+            resultVideo.push({
+              bitrate: z.bitrate,
+              content_type: z.content_type,
+              resolution: z.url.match(/([\d ]{2,5}[x][\d ]{2,5})/)[0],
+              url: z.url,
+            });
+          });
+          if (resultVideo.length !== 0) {
+            media.push({
+              type: v.type,
+              duration: v.duration,
+              thumbnail_url: v.thumbnail_url,
+              result: v.type === "video" ? resultVideo : v.url,
+            });
+          }
+        });
+      } else {
+        type = "photo";
+        response.data.tweet.media.photos.forEach((v) => {
+          media.push(v);
+        });
+      }
+
+      resolve({
+        status: "success",
+        result: {
+          id: response.data.tweet.id,
+          caption: response.data.tweet.text,
+          created_at: response.data.tweet.created_at,
+          created_timestamp: response.data.tweet.created_timestamp,
+          replies: response.data.tweet.replies,
+          retweets: response.data.tweet.retweets,
+          likes: response.data.tweet.likes,
+          url: response.data.tweet.url,
+          possibly_sensitive: response.data.tweet.possibly_sensitive,
+          author,
+          type,
+          media: media.length !== 0 ? media : null,
+        },
+      });
+  });
+};
