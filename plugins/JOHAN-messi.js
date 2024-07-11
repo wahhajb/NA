4f27be0670739507ae855055085e454564ef589f
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-11 04:13:08 +0300

    Update JOHAN-messi.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/JOHAN-messi.js
+++ plugins/JOHAN-messi.js
@@ -1,23 +1,73 @@
-const axios = require('axios');
+import fetch from 'node-fetch';
 
-let handler = async (m, { conn, args, command }) => {
-    try {
-        const response = await axios.get('https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=Lionel_Messi');
-        const player = response.data.player[0];
-        
-        // قم بتعديل النص والرابط ليتناسب مع المعلومات التي تريد إرسالها
-        let message = `لاعب كرة القدم الشهير: ${player.strPlayer}\n\n`;
-        message += `تاريخ الميلاد: ${player.dateBorn}\n`;
-        message += `مكان الميلاد: ${player.strBirthLocation}\n`;
-        message += `صورة اللاعب: ${player.strThumb}`;
+const handler = async (m, {text, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.adult_xnxxsearch
 
-        await conn.sendFile(m.chat, player.strThumb, 'player.jpg', message);
-    } catch (error) {
-        console.error('Error fetching player information:', error);
-        await m.reply('حدث خطأ أثناء جلب معلومات اللاعب.');
+
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${tradutor.texto1} ${usedPrefix}enable modohorny*`;   
+  if (!text) throw `${tradutor.texto2} ${usedPrefix + command} Con mi prima*`;
+  try {
+    const vids_ = {
+      from: m.sender,
+      urls: [],
+    };
+    if (!global.videoListXXX) {
+      global.videoListXXX = [];
     }
-}
+    if (global.videoListXXX[0]?.from == m.sender) {
+      global.videoListXXX.splice(0, global.videoListXXX.length);
+    }
+    const res = await xnxxsearch(text);
+    const json = res.result;
+    let cap = `${tradutor.texto3} ${text.toUpperCase()}\n\n`;
+    let count = 1;
+    for (const v of json) {
+      const linkXXX = v.link;
+      vids_.urls.push(linkXXX);
+      cap += `*[${count}]*\n• *🎬 Titulo:* ${v.title}\n• *🔗 Link:* ${v.link}\n• *❗ Info:* ${v.info}`;
+      cap += '\n\n' + '••••••••••••••••••••••••••••••••' + '\n\n';
+      count++;
+    }
+    m.reply(cap);
+    global.videoListXXX.push(vids_);
+  } catch {
+    throw e;
+  }
+};
+handler.help = ['xnxxsearch'].map((v) => v + ' <query>');
+handler.tags = ['downloader'];
+handler.command = /^عرب$/i;
+export default handler;
 
-handler.command = /^(football|كرة القدم|تاريخ كرة القدم)$/i
-handler.group = false
-module.exports = handler
+async function xnxxsearch(query) {
+  return new Promise((resolve, reject) => {
+    const baseurl = 'https://arabshentai.com';
+    fetch(`${baseurl}/search/${query}/${Math.floor(Math.random() * 3) + 1}`, {method: 'get'}).then((res) => res.text()).then((res) => {
+      const $ = cheerio.load(res, {xmlMode: false});
+      const title = [];
+      const url = [];
+      const desc = [];
+      const results = [];
+      $('div.mozaique').each(function(a, b) {
+        $(b).find('div.thumb').each(function(c, d) {
+          url.push(baseurl + $(d).find('a').attr('href').replace('/THUMBNUM/', '/'));
+        });
+      });
+      $('div.mozaique').each(function(a, b) {
+        $(b).find('div.thumb-under').each(function(c, d) {
+          desc.push($(d).find('p.metadata').text());
+          $(d).find('a').each(function(e, f) {
+            title.push($(f).attr('title'));
+          });
+        });
+      });
+      for (let i = 0; i < title.length; i++) {
+        results.push({title: title[i], info: desc[i], link: url[i]});
+      }
+      resolve({code: 200, status: true, result: results});
+    }).catch((err) => reject({code: 503, status: false, result: err}));
+  });
+    }
