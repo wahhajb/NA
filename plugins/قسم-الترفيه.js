6003a839a03955cd89b6ab61627c88bea531c3b5
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-13 03:23:26 +0300

    Update قسم-الترفيه.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/قسم-الترفيه.js
+++ plugins/قسم-الترفيه.js
@@ -1,7 +1,44 @@
-let handler = async (m, { command, text }) => m.reply(`*💥❯══⊹⊱≼ مرحبا بك ≽⊰⊹══❮💥*
-*❮ حط رمز قبل قسم حاجه من 👈  ( / . )❮*
+import { createHash } from 'crypto'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import fetch from 'node-fetch'
+import fs from 'fs'
+const { levelling } = '../lib/levelling.js'
+import moment from 'moment-timezone'
+import { promises } from 'fs'
+import { join } from 'path'
+const time = moment.tz('Egypt').format('HH')
+let wib = moment.tz('Egypt').format('HH:mm:ss')
+//import db from '../lib/database.js'
 
-*❮ اذا عندك طلب او اي شي تريد تقوله للمطور اكتب (.تقرير وحط طلبك)❮*
+let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    let d = new Date(new Date + 3600000)
+    let locale = 'ar'
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+let videoUrl = 'https://telegra.ph/file/b3475500b8374b806bf2d.mp4';
+  let vn = './media/menu.mp3';
+  const user = global.db.data.users[m.sender];
+  const {money, joincount} = global.db.data.users[m.sender];
+  const {exp, limit, level, role} = 
+    global.db.data.users[m.sender];
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+let username = conn.getName(who)
+let math = max - xp
+let sn = createHash('md5').update(who).digest('hex')
+let totalreg = Object.keys(global.db.data.users).length;
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(900) 
+  const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
+let str = `*≼مرحبا بك يا ${taguser} ≽*
+
+*》 حط رمز قبل قسم حاجه من 👈  ( / . )《*
+
+*》 اذا عندك طلب او اي شي تريد تقوله للمطور اكتب (.تقرير وحط طلبك)《*
 
 *❆❯══⊹⊱≼ 🥇 ≽⊰⊹══❮❆*
 *『 ️🥇 』⇦ قسم الترفيه ╿↶*
@@ -54,6 +91,9 @@ let handler = async (m, { command, text }) => m.reply(`*💥❯══⊹⊱≼ �
 
 *🎮┣.رابطي*
 ❍↜ييجيب لك رابطك
+
+🎮┣.كوبي*
+❍↜بينسخ لك نص من الصور (بس ضروري النص يكون en)
 
 *🎮┣.الكره*
 ❍↜يقيس نسبه الكره لك 
@@ -128,12 +168,40 @@ let handler = async (m, { command, text }) => m.reply(`*💥❯══⊹⊱≼ �
 ❍↜بيختار شخص عشوائي جميل
 
 *🎮┣تاج* 
-❍↜لعبه الصراحه`.trim(), null, m.mentionedJid ? {
-  mentions: m.mentionedJid
-} : {})
+❍↜لعبه الصراحه
+`.trim();
 
-handler.help = ['الاوامر <teks>?']
+conn.sendMessage(m.chat, {
+        video: { url: videoUrl }, caption: str,
+  mentions: [m.sender,global.conn.user.jid],
+  gifPlayback: true,gifAttribution: 0
+    }, { quoted: m });
+}; 
+handler.help = ['main']
 handler.tags = ['الاوامر', 'fun']
-handler.command = /^(قسم الترفيه|قسم-الترفيه|قسم-الترفية)$/i
+handler.command = ['قسم-الترفيه'] 
 
 export default handler
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
+    function ucapan() {
+      const time = moment.tz('Egypt').format('HH')
+      let res = "بداية يوم سعيده ☀️"
+      if (time >= 4) {
+        res = "صباح الخير 🌄"
+      }
+      if (time >= 10) {
+        res = "مساء الخير ☀️"
+      }
+      if (time >= 15) {
+        res = "مساء الخير 🌇"
+      }
+      if (time >= 18) {
+        res = "مساء الخير 🌙"
+      }
+      return res
+      }
