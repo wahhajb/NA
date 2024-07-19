5e391e499a4a02f4af5ae15956d1b705b4e76f90
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-20 02:44:16 +0300

    Update _similarity.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/_similarity.js
+++ plugins/_similarity.js
@@ -1,25 +1,28 @@
-
 import didyoumean from 'didyoumean'
-
 import similarity from 'similarity'
-import { plugins } from '../lib/plugins.js'
 
 export async function before(m, { conn, match, usedPrefix, command }) {
 	
-	     if ((usedPrefix = (match[0] || '')[0])) {
-		let noPrefix = m.text.replace(usedPrefix, '')
-		let args = noPrefix.trim().split` `.slice(1)
-		let text = args.join` `
-		let help = Object.values(plugins).filter(v => v.help && !v.disabled).map(v => v.help).flat(1)
-	       if (help.includes(noPrefix)) return
-		let mean = didyoumean(noPrefix, help)
-		let sim = similarity(noPrefix, mean)
-		let som = sim * 100
-		let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-		let name = await conn.getName(who)
-		
-		let caption = `اهلا يا ${username} لقد كتبت الامر بشكل خاطئ\n هل اردت ان تكتب هذا الامر  *${usedPrefix + mean} ?*`
- if (mean) this.reply(m.chat, `${caption}`, m)
-	    }
+if ((usedPrefix = (match[0] || '')[0])) {
+let noPrefix = m.text.replace(usedPrefix, '')
+let args = noPrefix.trim().split` `.slice(1)
+let text = args.join` `
+let help = Object.values(plugins).filter(v => v.help && !v.disabled).map(v => v.help).flat(1)
+if (help.includes(noPrefix)) return
+let mean = didyoumean(noPrefix, help)
+let sim = similarity(noPrefix, mean)
+let som = sim * 100
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = await conn.getName(who)
+let caption = `*👾 مرحبا* @${who.split('@')[0]}
+
+وفقًا لنظامي، هذه الوظيفة غير موجودة ولكنها تبدو مثل الوظائف التالية
+
+ 🌲 *${usedPrefix + mean}*
+ ❄️ *تشابه:* _${parseInt(som)}%_`
+if (mean) conn.reply(m.chat, caption, m, { mentions: [who]})
 }
-export const disabled = false
+
+}
+export const disabled = true
+      
