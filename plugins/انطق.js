6c66092fc1d48ca804f5b72cdaddc029c99297e1
AHMED7009 <135466958+AHMED7009@users.noreply.github.com>
2024-07-21 06:57:11 +0300

    Update انطق.js
    Signed-off-by: AHMED7009 <135466958+AHMED7009@users.noreply.github.com>

--- plugins/انطق.js
+++ plugins/انطق.js
@@ -1,39 +1,48 @@
-import gtts from 'node-gtts'
-import { readFileSync, unlinkSync } from 'fs'
-import { join } from 'path'
-const defaultLang = 'ar'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let lang = args[0]
-let text = args.slice(1).join(' ')
-if ((args[0] || '').length !== 2) { 
-lang = defaultLang 
-text = args.join(' ')
-}
-if (!text && m.quoted?.text) text = m.quoted.text
-let res
-try { res = await tts(text, lang) }
-catch (e) {
-m.reply(e + '')
-text = args.join(' ')
-if (!text) throw `*اكتب شيء وانا بقوله*`
-res = await tts(text, defaultLang)
-} finally {
-if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)    
-}}
-handler.help = ['tts <lang> <teks>']
-handler.tags = ['tools']
-handler.command = /^g?قول$/i
-export default handler
+import * as googleTTS from '@sefinek/google-tts-api'
+import {readFileSync, unlinkSync} from 'fs';
+import {join} from 'path';
+const defaultLang = 'ar';
+
+const handler = async (m, {conn, args, usedPrefix, command}) => {
+
+  let lang = args[0];
+  let text = args.slice(1).join(' ');
+  if ((args[0] || '').length !== 2) {
+    lang = defaultLang;
+    text = args.join(' ');
+  }
+  if (!text && m.quoted?.text) text = m.quoted.text;
+  let res;
+  try {
+    res = googleTTS.getAudioUrl(text, { lang: lang || 'ar', slow: false, host: 'https://translate.google.com' });
+  } catch (e) {
+    m.reply(e + '');
+    text = args.join(' ');
+    if (!text) throw `📌 مثال : \n${usedPrefix}${command} هينا اطلق بوت`;
+    res = await tts(text, defaultLang);
+  } finally {
+    if (res) {
+        conn.sendPresenceUpdate('recording', m.chat);
+        conn.sendMessage(m.chat, {audio: {url: res}, fileName: 'tts.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
+    }
+  }
+};
+handler.help = ['tts <lang> <teks>'];
+handler.tags = ['tools'];
+handler.command = /^انطق$/i;
+export default handler;
 
 function tts(text, lang = 'ar') {
-console.log(lang, text)
-return new Promise((resolve, reject) => {
-try {
-let tts = gtts(lang)
-let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
-tts.save(filePath, text, () => {
-resolve(readFileSync(filePath))
-unlinkSync(filePath)
-})
-} catch (e) { reject(e) }
-})}
+  return new Promise((resolve, reject) => {
+    try {
+      const tts = gtts(lang);
+      const filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav');
+      tts.save(filePath, text, () => {
+        resolve(readFileSync(filePath));
+        unlinkSync(filePath);
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
